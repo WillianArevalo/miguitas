8b6516b9c6819f58b2a6fbd26a1ba6f76702b945
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-10-12 22:06:03 -0600

    styles: update select custom for store

--- resources/views/components/select-store.blade.php
+++ resources/views/components/select-store.blade.php
@@ -8,16 +8,16 @@
     <input type="hidden" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}">
     <div class="relative">
         <div
-            class="selected @error($name) is-invalid @enderror flex w-full items-center justify-between rounded-xl border border-zinc-300 bg-white px-6 py-3 text-sm text-zinc-700 md:text-base">
-            <span class="itemSelected truncate" id="{{ $id }}_selected">
+            class="selected @error($name) is-invalid @enderror flex w-full items-center justify-between rounded-xl border-2 border-blue-store bg-white px-6 py-3 text-sm text-zinc-700 md:text-base">
+            <span class="itemSelected pluto-r truncate" id="{{ $id }}_selected">
                 {{ $selected && isset($options[$selected]) ? $options[$selected] : ($text ?: 'Seleccionar') }}
             </span>
             <x-icon icon="arrow-down" class="ms-4 h-5 w-5 text-zinc-500" />
         </div>
         <ul
-            class="selectOptions {{ count($options) > 6 ? 'h-64 overflow-auto' : '' }} an absolute z-10 mb-8 mt-2 hidden w-full rounded-xl border border-zinc-300 bg-white p-2 shadow-lg">
+            class="selectOptions {{ count($options) > 6 ? 'h-64 overflow-auto' : '' }} an absolute z-10 mb-8 mt-2 hidden w-full rounded-xl border-2 border-blue-store bg-white p-2 shadow-lg">
             @foreach ($options as $value => $label)
-                <li class="itemOption cursor-default truncate rounded-xl px-4 py-2.5 text-sm text-zinc-700 hover:bg-zinc-100 md:text-base"
+                <li class="itemOption pluto-r cursor-default truncate rounded-xl px-4 py-2.5 text-sm text-zinc-700 hover:bg-zinc-100 md:text-base"
                     data-value="{{ $value }}" data-input="#{{ $id }}">
                     {{ $label }}
                 </li>
