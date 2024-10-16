2f468eb8f5fa35e7ce9b657a0051d7df480e3fc0
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-10-16 16:07:32 -0600

    feat: add register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,82 +1,78 @@
 @extends('layouts.template')
 @section('title', 'Miguitas | Registro')
-@push('styles')
-    @vite('resources/css/store/login-signup.css')
-@endpush
 @section('content')
-    <div class="main-container">
-        <div class="form-container signup">
-            <div class="form-header">
-                <div class="rectangules">
-                    <div class="rectangule1"></div>
-                    <div class="rectangule2"></div>
+    <div class="m-0 md:p-4 lg:mx-10 lg:mb-10">
+        <div class="relative mx-auto flex h-full w-full flex-col-reverse overflow-hidden border md:flex-row xl:w-4/5">
+
+            <div class="absolute left-5 top-4 h-2 w-full bg-white md:left-10 md:top-8"></div>
+            <div class="absolute bottom-4 right-5 h-2 w-full bg-white md:bottom-8 md:right-10"></div>
+            <div class="absolute left-5 top-4 h-full w-2 bg-white md:left-10 md:top-8"></div>
+            <div class="absolute bottom-4 right-5 h-full w-2 bg-white md:bottom-8 md:right-10"></div>
+
+            <div class="flex flex-[2] flex-col items-center justify-center bg-light-blue py-20 md:ps-10">
+                <h1
+                    class="dine-r flex flex-col items-center justify-center text-xl font-bold text-light-gray sm:text-2xl md:text-4xl">
+                    Bienvenido a Miguitas
+                    <span class="mt-2 text-sm text-white">
+                        Crear una cuenta
+                    </span>
+                </h1>
+                <div class="my-4 flex items-center justify-center gap-4">
+                    <a href="">
+                        <x-icon-store icon="facebook" class="h-8 w-8 fill-white" />
+                    </a>
+                    <a href="{{ Route('auth.google') }}">
+                        <x-icon-store icon="google" class="h-8 w-8 fill-white" />
+                    </a>
+                    <a href="#">
+                        <x-icon-store icon="linkedin" class="h-8 w-8 fill-white" />
+                    </a>
                 </div>
-                <div class="square"></div>
-            </div>
-            <div class="form-content">
-                <div class="left-rectangule"></div>
-                <div class="container">
-
-                    <div class="form">
-                        <div class="header">
-                            <div class="title">
-                                <h1>Crea una cuenta</h1>
+                <div class="mt-4 w-4/5">
+                    <form action="{{ Route('login.validate') }}" method="POST">
+                        @csrf
+                        <div class="flex gap-4">
+                            <div class="flex flex-col gap-2">
+                                <x-input-store type="text" name="name" placeholder="Nombre" />
                             </div>
-                            <div class="icons">
-                                <svg width="28" height="28" viewBox="0 0 18 18" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M17.261 7.30904H8.93404V10.76H13.726C13.28 12.953 11.413 14.213 8.93404 14.213C8.24038 14.2142 7.5533 14.0785 6.91222 13.8135C6.27114 13.5486 5.68868 13.1597 5.19823 12.6691C4.70778 12.1786 4.31899 11.5961 4.05418 10.9549C3.78936 10.3138 3.65372 9.6267 3.65504 8.93304C3.65386 8.23946 3.78959 7.55246 4.05447 6.91145C4.31934 6.27044 4.70814 5.68801 5.19858 5.19758C5.68902 4.70714 6.27144 4.31834 6.91245 4.05346C7.55347 3.78859 8.24046 3.65285 8.93404 3.65404C10.193 3.65404 11.331 4.10104 12.224 4.83204L14.824 2.23304C13.24 0.852038 11.209 3.80581e-05 8.93404 3.80581e-05C7.75985 -0.00339412 6.59656 0.225348 5.51109 0.673108C4.42562 1.12087 3.43938 1.77881 2.6091 2.60909C1.77881 3.43938 1.12087 4.42562 0.673111 5.51109C0.225351 6.59656 -0.00339112 7.75985 4.10512e-05 8.93404C-0.00352385 10.1083 0.225128 11.2716 0.67284 12.3571C1.12055 13.4427 1.77849 14.429 2.6088 15.2593C3.43911 16.0896 4.4254 16.7475 5.51093 17.1952C6.59647 17.643 7.75981 17.8716 8.93404 17.868C13.401 17.868 17.463 14.619 17.463 8.93404C17.463 8.40604 17.382 7.83704 17.261 7.30904Z"
-                                        fill="white" />
-                                </svg>
-                                <svg width="30" height="30" viewBox="0 0 20 20" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M9.999 0C4.477 0 0 4.477 0 9.999C0 14.989 3.656 19.125 8.437 19.878V12.89H5.897V9.999H8.437V7.796C8.437 5.288 9.93 3.905 12.213 3.905C13.307 3.905 14.453 4.1 14.453 4.1V6.559H13.189C11.949 6.559 11.561 7.331 11.561 8.122V9.997H14.332L13.889 12.888H11.561V19.876C16.342 19.127 19.998 14.99 19.998 9.999C19.998 4.477 15.521 0 9.999 0Z"
-                                        fill="white" />
-                                </svg>
-                                <svg width="28" height="28" viewBox="0 0 18 18" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M17 0H1C0.734784 0 0.48043 0.105357 0.292893 0.292893C0.105357 0.48043 0 0.734784 0 1V17C0 17.2652 0.105357 17.5196 0.292893 17.7071C0.48043 17.8946 0.734784 18 1 18H17C17.2652 18 17.5196 17.8946 17.7071 17.7071C17.8946 17.5196 18 17.2652 18 17V1C18 0.734784 17.8946 0.48043 17.7071 0.292893C17.5196 0.105357 17.2652 0 17 0ZM5.339 15.337H2.667V6.747H5.339V15.337ZM4.003 5.574C3.59244 5.574 3.1987 5.41091 2.9084 5.1206C2.61809 4.8303 2.455 4.43656 2.455 4.026C2.455 3.61544 2.61809 3.22171 2.9084 2.9314C3.1987 2.64109 3.59244 2.478 4.003 2.478C4.41355 2.478 4.80729 2.64109 5.0976 2.9314C5.38791 3.22171 5.551 3.61544 5.551 4.026C5.551 4.43656 5.38791 4.8303 5.0976 5.1206C4.80729 5.41091 4.41355 5.574 4.003 5.574ZM15.338 15.337H12.669V11.16C12.669 10.164 12.651 8.883 11.281 8.883C9.891 8.883 9.68 9.969 9.68 11.09V15.338H7.013V6.748H9.573V7.922H9.61C9.965 7.247 10.837 6.535 12.134 6.535C14.838 6.535 15.337 8.313 15.337 10.627L15.338 15.337Z"
-                                        fill="white" />
-                                </svg>
-
+                            <div class="flex flex-col gap-2">
+                                <x-input-store type="text" name="last_name" placeholder="Apellido" />
                             </div>
                         </div>
-                        <form action="">
-                            <div class="inputs">
-                                <input class="" type="text" name="" id="" placeholder="Nombre">
-                                <input class="" type="email" name="" id="" placeholder="Correo">
-                                <input type="password" name="" id="" placeholder="Contraseña">
+                        <div class="flex flex-col gap-2">
+                            <x-input-store type="email" name="email" placeholder="Correo eletrónico" icon="email" />
+                        </div>
+                        <div class="flex gap-4">
+                            <div class="flex flex-col gap-2">
+                                <x-input-store type="password" name="password" placeholder="Contraseña" icon="key" />
                             </div>
-                            <div class="actions">
-                                <button type="submit">Regístrate</button>
+                            <div class="flex flex-col gap-2">
+                                <x-input-store type="password" name="password" placeholder="Confirmar" />
                             </div>
-                        </form>
-                    </div>
-                    <div class="info">
-                        <h1>¡Hola
-                            Amig@!
-                        </h1>
-                        <p>Bienvenido al
-                            mundo donde se
-                            cumplen los deseos
-                            de tu mejor amigo
-                            peludo
-                        </p>
-                    </div>
+                        </div>
+                        <div class="mt-4 flex flex-col items-center justify-center gap-4">
+                            <x-button-store type="submit" text="Registrarse" typeButton="primary" />
+                            <p class="pluto-r text-white">
+                                ¿Ya tienes una cuenta?
+                                <a href="{{ Route('login') }}" class="dine-r text-white underline">
+                                    Iniciar sesión
+                                </a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
-                </div>
-                <div class="right-rectangule"></div>
+
+            <div class="flex flex-1 flex-col items-center justify-center gap-2 bg-light-pink py-10 md:pe-20 md:ps-10">
+                <h2 class="pt-10 text-center text-xl font-semibold text-dark-pink md:pt-0 md:text-2xl">
+                    ¡Hola amig@!
+                </h2>
+                <p class="w-1/2 text-center text-sm text-dark-blue md:w-full md:text-base">
+                    Registrate y cumple los antojos de tu mascota con los mejores productos de la región.
+                </p>
             </div>
-            <div class="form-footer">
-                <div class="square"></div>
-                <div class="rectangules">
-                    <div class="rectangule1"></div>
-                    <div class="rectangule2"></div>
-                </div>
-            </div>
+
         </div>
     </div>
 @endsection
