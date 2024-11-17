8a40ef81ab656083f8b799ece1d33d2aa8fa753c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-17 17:04:18 -0600

    styles: update toast in the store

--- resources/views/layouts/__partials/store/toast-store.blade.php
+++ resources/views/layouts/__partials/store/toast-store.blade.php
@@ -4,21 +4,25 @@
         role="alert">
         <div class="inline-flex flex-shrink-0 items-center justify-center">
             @if ($message = Session::get('success'))
-                <x-icon-store icon="circle-check" class="h-6 w-6 fill-green-600" />
+                <x-icon-store icon="circle-check" class="h-6 w-6 text-green-500" />
             @elseif($message = Session::get('error'))
-                <x-icon-store icon="circle-exclamation" class="h-6 w-6 fill-red-600" />
+                <x-icon-store icon="circle-exclamation" class="h-6 w-6 text-red-500" />
             @elseif($message = Session::get('info'))
-                <x-icon-store icon="circle-info" class="h-6 w-6 fill-blue-600" />
+                <x-icon-store icon="circle-info" class="h-6 w-6 text-blue-500" />
             @endif
         </div>
-        <div class="ms-auto text-center text-xs font-normal sm:text-sm">
+        <div class="font-diner-r ms-auto text-center text-xs font-normal sm:text-sm">
             {{ $message }}
         </div>
         <button type="button"
             class="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 items-center justify-center rounded-xl bg-white text-zinc-400 hover:bg-zinc-100 hover:text-zinc-900 focus:ring-2 focus:ring-zinc-300"
             data-dismiss-target="#toast" aria-label="Close">
             <span class="sr-only">Close</span>
-            <x-icon-store icon="close" class="h-4 w-4 fill-current" />
+            <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
         </button>
     </div>
 @endif
