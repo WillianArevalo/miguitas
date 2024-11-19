c6b22045e20dd9e94db31d626cd5122932ae344f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-19 16:51:36 -0600

    feat: update styles for not found products

--- resources/views/store/products.blade.php
+++ resources/views/store/products.blade.php
@@ -161,12 +161,22 @@
                     </div>
                 </aside>
             </div>
-            <div class="flex-[3]">
-                <div class="grid grid-cols-3 gap-4 px-2 max-[840px]:grid-cols-2 xl:grid-cols-3">
-                    @foreach ($products as $product)
-                        <x-card-product :product="$product" />
-                    @endforeach
-                </div>
+            <div class="flex-[3] px-4">
+                @if ($products->count() > 0)
+                    <div class="grid grid-cols-3 gap-4 px-2 max-[840px]:grid-cols-2 xl:grid-cols-3">
+                        @foreach ($products as $product)
+                            <x-card-product :product="$product" />
+                        @endforeach
+                    </div>
+                @else
+                    <div
+                        class="flex h-full flex-col items-center justify-center rounded-2xl border-2 border-dashed border-zinc-300 p-10 px-4">
+                        <x-icon-store icon="sad" class="mb-4 h-12 w-12 text-zinc-500" />
+                        <p class="flex items-center gap-2 text-center text-base text-zinc-500">
+                            No se encontraron productos con los filtros aplicados, <br> intenta con otros filtros.
+                        </p>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
