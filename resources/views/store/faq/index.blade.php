1e80c44889a052758d98f15f08851e1f812d3901
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-19 12:50:48 -0600

    feat: add logic to display faq and faqs categories

--- resources/views/store/faq/index.blade.php
+++ resources/views/store/faq/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.template')
-@section('title', 'Preguntas frecuentes')
-
+@section('title', 'Miguitas | Preguntas frecuentes')
 @section('content')
     <div class="main-container">
         <div class="w-full py-20 text-center" style="background-image: url({{ asset('img/bg-image.png') }});">
@@ -9,65 +8,43 @@
             </h1>
         </div>
         <div class="flex flex-col">
-            <div class="my-10 flex flex-col items-start justify-center gap-10 px-4 sm:justify-between md:flex-row">
-                <div class="flex flex-1 items-center justify-center sm:justify-end">
-                    <h1 class="text-4xl text-blue-store">
-                        Cuenta
-                    </h1>
-                </div>
-                <section class="flex flex-[2] flex-col gap-8 px-4 text-sm text-zinc-700 sm:px-10 md:text-base">
-                    @for ($i = 0; $i < 4; $i++)
-                        <div class="">
-                            <button
-                                class="accordion-header flex w-full items-center justify-start gap-2 text-base md:text-lg"
-                                data-target="#panel-{{ $i }}">
-                                <x-icon-store icon="caret-right" class="h-5 w-5 text-zinc-800 transition-transform" />
-                                Descripción
-                            </button>
-                            <div id="panel-{{ $i }}"
-                                class="hidden animate-fade-down transition-all animate-duration-300">
-                                <div class="p-6">
-                                    <p class="text-sm text-zinc-700 sm:text-base">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas blanditiis commodi
-                                        omnis necessitatibus temporibus repellendus, aspernatur ducimus sint assumenda
-                                        deleniti quia voluptates, esse, ipsum impedit quo quas quos ea numquam.
-                                    </p>
+            @php
+                $groupedFaqs = $faqs->groupBy(function ($faq) {
+                    return $faq->category->name;
+                });
+            @endphp
+            @foreach ($groupedFaqs as $categoryName => $faqs)
+                <div class="my-10 flex flex-col items-start justify-center gap-10 px-4 sm:justify-between md:flex-row">
+                    <div class="flex flex-1 items-center justify-center sm:justify-end">
+                        <h1 class="text-4xl text-blue-store">
+                            {{ $categoryName }}
+                        </h1>
+                    </div>
+                    <section class="flex flex-[2] flex-col gap-8 px-4 text-sm text-zinc-700 sm:px-10 md:text-base">
+                        @foreach ($faqs as $faq)
+                            <div class="">
+                                <button
+                                    class="accordion-header font-font-dine-r flex w-full items-center justify-start gap-2 text-base md:text-lg"
+                                    data-target="#panel-{{ $faq->id }}">
+                                    <x-icon-store icon="caret-right" class="h-5 w-5 text-zinc-800 transition-transform" />
+                                    {{ $faq->question }}
+                                </button>
+                                <div id="panel-{{ $faq->id }}"
+                                    class="hidden animate-fade-down transition-all animate-duration-300">
+                                    <div class="p-6">
+                                        <p class="font-font-pluto-r text-sm text-zinc-700 sm:text-base">
+                                            {{ $faq->answer }}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endfor
-                </section>
-            </div>
-            <div class="line"></div>
-            <div class="my-10 flex flex-col items-start justify-center gap-10 px-4 sm:justify-between md:flex-row">
-                <div class="flex flex-1 items-center justify-center sm:justify-end">
-                    <h1 class="text-4xl text-blue-store">
-                        Ordenes
-                    </h1>
+                        @endforeach
+                    </section>
                 </div>
-                <section class="flex flex-[2] flex-col gap-8 px-4 text-sm text-zinc-700 sm:px-10 md:text-base">
-                    @for ($i = 0; $i < 4; $i++)
-                        <div class="">
-                            <button
-                                class="accordion-header flex w-full items-center justify-start gap-2 text-base md:text-lg"
-                                data-target="#panel-two-{{ $i }}">
-                                <x-icon-store icon="caret-right" class="h-5 w-5 text-zinc-800 transition-transform" />
-                                Descripción
-                            </button>
-                            <div id="panel-two-{{ $i }}"
-                                class="hidden animate-fade-down transition-all animate-duration-300">
-                                <div class="p-6">
-                                    <p class="text-sm text-zinc-700 sm:text-base">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas blanditiis commodi
-                                        omnis necessitatibus temporibus repellendus, aspernatur ducimus sint assumenda
-                                        deleniti quia voluptates, esse, ipsum impedit quo quas quos ea numquam.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    @endfor
-                </section>
-            </div>
+                @if (!$loop->last)
+                    <div class="line"></div>
+                @endif
+            @endforeach
         </div>
     </div>
 @endsection
