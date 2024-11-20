1ca2d5635fb8dfacf8471509ed823570bbe840cf
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-20 11:28:09 -0600

    styles: add text responsive

--- resources/views/components/head-band.blade.php
+++ resources/views/components/head-band.blade.php
@@ -1,14 +1,15 @@
 <div class="flex justify-center py-2">
     @if ($headBand->link_text)
-        <h2 class="font-font-dine-r px-4 text-base font-medium text-white">
+        <h2 class="font-font-dine-r px-4 text-xs font-medium text-white sm:text-base">
             {{ $headBand->title }}
             <a href="{{ $headBand->link }}"
-                class="font-font-dine-r ms-4 text-sm font-normal text-white underline hover:text-zinc-900">
+                class="font-font-dine-r ms-4 text-xs font-normal text-white underline hover:text-zinc-900 sm:text-sm">
                 {{ $headBand->link_text }}
             </a>
         </h2>
     @else
-        <a href="{{ $headBand->link }}" class="font-font-dine-r px-4 text-base font-medium text-white hover:underline">
+        <a href="{{ $headBand->link }}"
+            class="font-font-dine-r px-4 text-xs font-medium text-white hover:underline sm:text-base">
             {{ $headBand->title }}
         </a>
     @endif
