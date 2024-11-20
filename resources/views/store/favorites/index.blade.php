ecad1565df7a2c248c0dd4169ec17b42a73fa3bc
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-20 11:29:00 -0600

    feat: add slider products

--- resources/views/store/favorites/index.blade.php
+++ resources/views/store/favorites/index.blade.php
@@ -71,7 +71,7 @@
                 </div>
             @else
                 <div class="mx-auto flex flex-col items-center justify-center p-20">
-                    <h2 class="text-2xl text-gray-store">
+                    <h2 class="text-center text-gray-store sm:text-lg md:text-xl lg:text-2xl">
                         ¡No tienes productos favoritos!
                     </h2>
                     <x-button-store type="a" href="{{ Route('store') }}" text="Ir a la tienda" typeButton="primary"
@@ -79,7 +79,6 @@
                 </div>
             @endif
         </div>
-
         <div class="my-10 px-0 sm:px-4 md:px-10">
             <h2 class="my-4 border-b-4 border-blue-store text-4xl text-blue-store">
                 ¡Encuentra aquí tus favoritos!
@@ -87,59 +86,14 @@
             <div class="">
                 <div class="swiper mySwiper w-100 h-full px-4">
                     <div class="swiper-wrapper pb-10">
-                        @for ($i = 0; $i < 8; $i++)
-                            <div class="swiper-slide relative rounded-3xl border border-zinc-200 p-2 shadow-xl sm:p-6">
-                                <div class="card-image">
-                                    <img src="{{ asset('img/image.jpg') }}" alt="Featured2 image"
-                                        class="h-48 w-full rounded-xl object-cover md:h-60">
-                                </div>
-                                <div class="card-body mt-4">
-                                    <div class="flex items-center justify-between">
-                                        <div class="flex items-center gap-3">
-                                            <a href="">
-                                                <x-icon-store icon="heart"
-                                                    class="h-5 w-5 fill-blue-store sm:h-7 sm:w-7" />
-                                            </a>
-                                            <a href="">
-                                                <x-icon-store icon="comment"
-                                                    class="h-5 w-5 fill-blue-store sm:h-7 sm:w-7" />
-                                            </a>
-                                            <a href="">
-                                                <x-icon-store icon="send"
-                                                    class="h-5 w-5 fill-blue-store sm:h-7 sm:w-7" />
-                                            </a>
-                                        </div>
-                                        <button>
-                                            <x-icon-store icon="cart" class="h-5 w-5 fill-blue-store sm:h-7 sm:w-7" />
-                                        </button>
-                                    </div>
-                                    <div class="pb-6">
-                                        <small class="mt-2 block text-start">
-                                            <p class="font-pluto-m text-xs text-gray-store sm:text-sm">13,355 view</p>
-                                        </small>
-                                        <h2
-                                            class="text-start font-pluto-r text-sm font-semibold text-blue-store sm:text-base md:text-lg">
-                                            #Cake Corazón FurryLove
-                                        </h2>
-                                        <p class="text-start">
-                                            <span class="font-dine-r text-lg text-gray-store">$</span>
-                                            <span class="font-dine-r text-lg text-gray-store">25.00</span>
-                                        </p>
-                                    </div>
-                                </div>
-                                <a href=""
-                                    class="absolute bottom-0 right-0 m-2 rounded-full border-2 border-blue-store bg-pink-store p-2 sm:m-4">
-                                    <x-icon-store icon="arrow-right" class="h-5 w-5 fill-blue-store sm:h-7 sm:w-7" />
-                                </a>
-                            </div>
-                        @endfor
+                        @foreach ($products as $product)
+                            <x-card-product2 :product="$product" />
+                        @endforeach
                     </div>
                     <div class="swiper-pagination"></div>
                 </div>
             </div>
         </div>
-
-
         <div class="mx-auto mb-10 w-full px-4 md:w-3/4">
             <img src="{{ asset('img/mango-discount.png') }}" alt="">
         </div>
