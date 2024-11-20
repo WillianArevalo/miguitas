87468ffa787696242c4b511cd0fc3fc4708e5fd8
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-19 20:16:44 -0600

    feat: add function to create or update the information of the store

--- app/Http/Controllers/Admin/SettingsGeneralController.php
+++ app/Http/Controllers/Admin/SettingsGeneralController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helpers\ImageHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Settings;
+use App\Models\SocialLink;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,7 +14,41 @@ class SettingsGeneralController extends Controller
     public function index()
     {
         $maintenance = Settings::where('key', 'site_in_maintenance')->first();
-        return view('admin.general_settings.index', compact('maintenance'));
+        $settings = Settings::all();
+        $socialLinks = SocialLink::all();
+        return view('admin.general_settings.index', compact('maintenance', 'settings', 'socialLinks'));
+    }
+
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+
+            foreach ($request->all() as $key => $value) {
+                if ($key == '_token' || $key == '_method') continue;
+
+                if ($key == "store_logo" && $request->hasFile("store_logo")) {
+                    $file = $request->file("store_logo");
+                    $value = ImageHelper::saveImage($file, 'images/settings');
+                }
+
+                if ($key == "store_favicon" && $request->hasFile("store_favicon")) {
+                    $file = $request->file("store_favicon");
+                    $value = ImageHelper::saveImage($file, 'images/settings');
+                }
+
+                Settings::updateOrCreate(
+                    ['key' => $key],
+                    ['value' => $value]
+                );
+            }
+
+            DB::commit();
+            return redirect()->route('admin.general-settings.index')->with('success', 'Datos actualizados correctamente');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('admin.general-settings.index')->with('error', 'Error al actualizar los datos');
+        }
     }
 
     public function maintenanceUpdate(Request $request)
