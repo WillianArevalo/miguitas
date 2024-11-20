e1d1380df02a46f2b9c305d75dd86f5bf800cfd3
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-19 21:31:22 -0600

    feat: update account js

--- resources/js/store/account.js
+++ resources/js/store/account.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    $(".tab-btn").click(function () {
+        $(".tab-btn").removeClass("active-tab");
+        $(this).addClass("active-tab");
+        $(".tab-panel").addClass("hidden");
+        const target = $(this).data("target");
+        $(target).removeClass("hidden");
+    });
+
     $(".add-address").on("click", function () {
         $("#container-address-empty").hide();
         $("#container-address-list").hide();
