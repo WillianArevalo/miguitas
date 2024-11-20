d4c69308bc54db192d12661e2c1084ca490fdf24
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-19 20:20:15 -0600

    feat: add favicon dinamic

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" href="{{ asset('images/imagen6.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ $favicon ? Storage::url($favicon) : asset('images/logo.png') }}"
+        type="image/x-icon">
     @vite('resources/css/store.css')
     @stack('styles')
     @vite('resources/js/app.js')
