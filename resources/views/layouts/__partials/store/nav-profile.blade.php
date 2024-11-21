0c4dcb4a58a85b2c61928b23d00b5112b1285ff9
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-20 21:31:00 -0600

    styles: update nav profile

--- resources/views/layouts/__partials/store/nav-profile.blade.php
+++ resources/views/layouts/__partials/store/nav-profile.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-auto flex w-max justify-center sm:hidden">
     <a href="{{ Route('account.index') }}" data-tooltip-target="tooltip-general"
-        class="link-profile {{ Route::is('account.index') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
+        class="link-profile {{ Route::is('account.index') ? 'active' : '' }} relative flex items-center px-2 py-4 ps-4 text-blue-store">
         <x-icon-store icon="home" class="me-2 h-5 w-5 text-current" />
         <span class="hidden xl:block">General</span>
     </a>
@@ -11,7 +11,7 @@
     </div>
 
     <a href="{{ Route('orders.index') }}" data-tooltip-target="tooltip-orders"
-        class="link-profile {{ Route::is('orders') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
+        class="link-profile {{ Route::is('orders') ? 'active' : '' }} relative flex items-center px-2 py-4 ps-4 text-blue-store">
         <x-icon-store icon="shopping-bag" class="me-2 h-5 w-5 text-current" />
         <span class="hidden xl:block">Pedidos</span>
     </a>
@@ -22,7 +22,7 @@
     </div>
 
     <a href="#" data-tooltip-target="tooltip-payments"
-        class="link-profile relative flex items-center p-2 ps-4 text-secondary">
+        class="link-profile relative flex items-center px-2 py-4 ps-4 text-blue-store">
         <x-icon-store icon="credit-card" class="me-2 h-5 w-5 text-current" />
         <span class="hidden xl:block">Pagos</span>
     </a>
@@ -33,7 +33,7 @@
     </div>
 
     <a href="{{ Route('account.settings') }}" data-tooltip-target="tooltip-settings"
-        class="link-profile {{ Route::is('account.settings') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
+        class="link-profile {{ Route::is('account.settings') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-blue-store">
         <x-icon-store icon="settings" class="me-2 h-5 w-5 text-current" />
         <span class="hidden xl:block">Configuración</span>
     </a>
@@ -44,7 +44,7 @@
     </div>
 
     <a href="{{ Route('account.addresses.index') }}" data-tooltip-target="tooltip-addresses"
-        class="{{ Route::is('account.addresses.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-secondary">
+        class="{{ Route::is('account.addresses.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-blue-store">
         <x-icon-store icon="location" class="me-2 h-5 w-5 text-current" />
         <span class="hidden xl:block">Direcciones</span>
     </a>
@@ -55,7 +55,7 @@
     </div>
 
     <a href="{{ Route('account.tickets.index') }}" data-tooltip-target="tooltip-tickets"
-        class="{{ Route::is('account.tickets.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-secondary">
+        class="{{ Route::is('account.tickets.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-blue-store">
         <x-icon-store icon="ticket-02" class="me-2 h-5 w-5 text-current" />
         <span class="hidden xl:block">Tickets</span>
     </a>
@@ -67,39 +67,47 @@
 </div>
 
 <div class="hidden sm:flex sm:flex-col">
-    <h2 class="flex items-center gap-2 px-4 pt-4 font-league-spartan text-2xl font-bold text-secondary">
-        <x-icon-store icon="user" class="h-5 w-5 text-current" />
-        <span class="hidden xl:block">Mi cuenta</span>
-    </h2>
-    <div class="mb-4 mt-4 flex flex-col xl:mt-0">
+    <div class="flex items-center gap-4 border-b-2 border-zinc-200 p-4">
+        <img src="{{ Storage::url($user->profile) }}" alt="Imagen de {{ $user->full_name }}"
+            class="h-20 w-20 rounded-full object-cover">
+        <div class="flex flex-col justify-center gap-1">
+            <h2 class="text-lg font-bold text-blue-store">{{ $user->full_name }}</h2>
+            <p class="font-dine-r text-sm text-zinc-500">{{ $user->email }}</p>
+        </div>
+    </div>
+    <div class="mb-4 mt-4 flex flex-col pt-4 xl:mt-0">
         <a href="{{ Route('account.index') }}"
-            class="link-profile {{ Route::is('account.index') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
+            class="link-profile {{ Route::is('account.index') ? 'bg-blue-store rounded-xl text-white' : '' }} relative flex items-center px-2 py-4 ps-4 text-blue-store">
             <x-icon-store icon="home" class="me-2 h-5 w-5 text-current" />
             <span class="hidden xl:block">General</span>
         </a>
         <a href="{{ Route('orders.index') }}"
-            class="link-profile {{ Route::is('orders') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
-            <x-icon-store icon="shopping-bag" class="me-2 h-5 w-5 text-current" />
+            class="link-profile {{ Route::is('orders.index') ? 'bg-blue-store rounded-xl text-white' : '' }} relative flex items-center px-2 py-4 ps-4 text-blue-store">
+            <x-icon-store icon="bag" class="me-2 h-5 w-5 text-current" />
             <span class="hidden xl:block">Pedidos</span>
         </a>
-        <a href="#" class="link-profile relative flex items-center p-2 ps-4 text-secondary">
-            <x-icon-store icon="credit-card" class="me-2 h-5 w-5 text-current" />
+        <a href="#" class="link-profile relative flex items-center px-2 py-4 ps-4 text-blue-store">
+            <x-icon-store icon="payment" class="me-2 h-5 w-5 text-current" />
             <span class="hidden xl:block">Pagos</span>
         </a>
-        <a href="{{ Route('account.settings') }}"
-            class="link-profile {{ Route::is('account.settings') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
-            <x-icon-store icon="settings" class="me-2 h-5 w-5 text-current" />
-            <span class="hidden xl:block">Configuración</span>
+        <a href="{{ Route('cancel-return') }}"
+            class="link-profile {{ Route::is('cancel-return') ? 'bg-blue-store rounded-xl text-white' : '' }} relative flex items-center px-2 py-4 ps-4 text-blue-store">
+            <x-icon-store icon="return-arrow" class="me-2 h-5 w-5 text-current" />
+            <span class="hidden xl:block">
+                Cancelaciones y devoluciones
+            </span>
         </a>
         <a href="{{ Route('account.addresses.index') }}"
-            class="{{ Route::is('account.addresses.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-secondary">
+            class="{{ Route::is('account.addresses.index') || Route::is('account.addresses.create') || Route::is('account.addresses.edit') ? 'bg-blue-store rounded-xl text-white' : '' }} link-profile relative flex items-center px-2 py-4 ps-4 text-blue-store">
             <x-icon-store icon="location" class="me-2 h-5 w-5 text-current" />
             <span class="hidden xl:block">Direcciones</span>
         </a>
         <a href="{{ Route('account.tickets.index') }}"
-            class="{{ Route::is('account.tickets.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-secondary">
-            <x-icon-store icon="ticket-02" class="me-2 h-5 w-5 text-current" />
-            <span class="hidden xl:block">Tickets</span>
+            class="{{ Route::is('account.tickets.index') ? 'bg-blue-store rounded-xl text-white' : '' }} link-profile relative flex items-center px-2 py-4 ps-4 text-blue-store">
+            <x-icon-store icon="headpones" class="me-2 h-5 w-5 text-current" />
+            <span class="hidden xl:block">
+                Soporte técnico
+            </span>
         </a>
     </div>
 </div>
