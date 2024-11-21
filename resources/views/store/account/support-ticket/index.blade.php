cb14ebcb17ec5b0e29a0b38a22450d87b1706a1a
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-20 21:30:37 -0600

    styles: update styles to support ticket index

--- resources/views/store/account/support-ticket/index.blade.php
+++ resources/views/store/account/support-ticket/index.blade.php
@@ -2,41 +2,40 @@
 @section('profile-content')
     <div class="flex flex-col">
         <div class="pb-3">
-            <h2 class="font-league-spartan text-3xl font-bold text-secondary">
+            <h2 class="text-3xl font-bold text-blue-store">
                 Soporte técnico
             </h2>
         </div>
-        <div class="mb-4 border-t border-zinc-400">
+        <div class="mb-4 border-t-2 border-zinc-200">
             <div class="mt-4 flex justify-end">
-                <x-button-store type="a" href="{{ Route('account.tickets.create') }}" text="Crear ticket"
-                    icon="customer-service" typeButton="primary" class="w-max font-bold" />
+                <x-button-store type="a" href="{{ Route('account.tickets.create') }}" text="Crear ticket" icon="plus"
+                    typeButton="primary" class="w-max" />
             </div>
             <div class="mt-4">
                 @if ($tickets->count() > 0)
                     <div
-                        class="hidden flex-col items-center justify-center gap-2 rounded-xl border border-zinc-200 px-4 pb-4 shadow-sm lg:flex">
-                        <table class="w-full font-league-spartan">
+                        class="hidden flex-col items-center justify-center gap-2 rounded-xl border border-zinc-200 px-4 shadow-sm lg:flex">
+                        <table class="w-full table-auto font-dine-r">
                             <thead>
-                                <tr class="border-b border-zinc-400">
+                                <tr class="border-b border-zinc-200">
                                     <th scope="col"
-                                        class="px-4 py-3 text-left font-league-spartan text-xs font-medium uppercase tracking-wider text-zinc-500">
+                                        class="p-4 text-left font-dine-r text-xs font-medium uppercase tracking-wider text-zinc-500">
                                         Número de ticket
                                     </th>
-
                                     <th scope="col"
-                                        class="px-4 py-3 text-left font-league-spartan text-xs font-medium uppercase tracking-wider text-zinc-500">
+                                        class="p-4 text-left font-dine-r text-xs font-medium uppercase tracking-wider text-zinc-500">
                                         Asunto
                                     </th>
                                     <th scope="col"
-                                        class="px-4 py-3 text-left font-league-spartan text-xs font-medium uppercase tracking-wider text-zinc-500">
+                                        class="p-4 text-left font-dine-r text-xs font-medium uppercase tracking-wider text-zinc-500">
                                         Prioridad
                                     </th>
                                     <th scope="col"
-                                        class="px-4 py-3 text-left font-league-spartan text-xs font-medium uppercase tracking-wider text-zinc-500">
+                                        class="p-4 text-left font-dine-r text-xs font-medium uppercase tracking-wider text-zinc-500">
                                         Estado
                                     </th>
                                     <th scope="col"
-                                        class="px-4 py-3 text-left font-league-spartan text-xs font-medium uppercase tracking-wider text-zinc-500">
+                                        class="p-4 text-left font-dine-r text-xs font-medium uppercase tracking-wider text-zinc-500">
                                         Acciones
                                     </th>
                                 </tr>
@@ -46,7 +45,7 @@
                                     <tr class="hover:bg-zinc-50">
                                         <td class="whitespace-nowrap px-4 py-4">
                                             <span
-                                                class="font-secondary rounded-full bg-blue-100 px-4 py-1 text-sm font-semibold text-secondary">
+                                                class="font-secondary rounded-full bg-purple-100 px-4 py-1 text-sm text-blue-store">
                                                 {{ $ticket->ticket_number }}
                                             </span>
                                         </td>
@@ -57,7 +56,7 @@
                                         </td>
                                         <td class="whitespace-nowrap px-4 py-4">
                                             <span
-                                                class="rounded-full bg-red-100 px-4 py-1 text-xs font-medium text-red-700">
+                                                class="rounded-full bg-red-100 px-4 py-1 font-dine-b text-xs font-medium text-red-700">
                                                 {{ ucfirst($ticket->priority) }}
                                             </span>
                                         </td>
@@ -66,35 +65,35 @@
                                             @switch($ticket->status)
                                                 @case('open')
                                                     <span
-                                                        class="rounded-full bg-blue-100 px-4 py-1 text-xs font-medium text-blue-700">
+                                                        class="rounded-full bg-blue-100 px-4 py-1 font-dine-b text-xs font-medium text-blue-700">
                                                         {{ ucfirst($ticket->status) }}
                                                     </span>
                                                 @break
 
                                                 @case('pending')
                                                     <span
-                                                        class="rounded-full bg-yellow-100 px-4 py-1 text-xs font-medium text-yellow-700">
+                                                        class="rounded-full bg-yellow-100 px-4 py-1 font-dine-b text-xs font-medium text-yellow-700">
                                                         {{ ucfirst($ticket->status) }}
                                                     </span>
                                                 @break
 
                                                 @case('resolved')
                                                     <span
-                                                        class="rounded-full bg-green-100 px-4 py-1 text-xs font-medium text-green-700">
+                                                        class="rounded-full bg-green-100 px-4 py-1 font-dine-b text-xs font-medium text-green-700">
                                                         {{ ucfirst($ticket->status) }}
                                                     </span>
                                                 @break
 
                                                 @case('reopend')
                                                     <span
-                                                        class="rounded-full bg-orange-100 px-4 py-1 text-xs font-medium text-orange-700">
+                                                        class="rounded-full bg-orange-100 px-4 py-1 font-dine-b text-xs font-medium text-orange-700">
                                                         {{ ucfirst($ticket->status) }}
                                                     </span>
                                                 @break
 
                                                 @case('closed')
                                                     <span
-                                                        class="rounded-full bg-red-100 px-4 py-1 text-xs font-medium text-red-700">
+                                                        class="rounded-full bg-red-100 px-4 py-1 font-dine-b text-xs font-medium text-red-700">
                                                         {{ ucfirst($ticket->status) }}
                                                     </span>
                                                 @break
@@ -104,7 +103,7 @@
                                         </td>
                                         <td class="whitespace-nowrap px-4 py-4 text-sm">
                                             <div class="flex items-center gap-2">
-                                                <x-button-store icon="view" type="a" href="#"
+                                                <x-button-store icon="eye" type="a" href="#"
                                                     typeButton="secondary" onlyIcon="true" class="w-max" />
                                                 <x-button-store icon="comment" type="a" href="#"
                                                     typeButton="secondary" onlyIcon="true" class="w-max" />
@@ -112,13 +111,13 @@
                                                     <form action="{{ Route('account.tickets.close', $ticket->id) }}"
                                                         method="POST" id="formCancelTicket-{{ $ticket->id }}">
                                                         @csrf
-                                                        <x-button-store icon="cancel-circle" type="button"
+                                                        <x-button-store icon="trash" type="button"
                                                             href="{{ Route('account.tickets.show', $ticket->id) }}"
                                                             typeButton="danger" onlyIcon="true" class="buttonDelete w-max"
-                                                            data-tooltip-target="tooltip-cancel-ticket"
+                                                            data-tooltip-target="tooltip-cancel-ticket-{{ $ticket->id }}"
                                                             data-form="formCancelTicket-{{ $ticket->id }}" />
                                                     </form>
-                                                    <div id="tooltip-cancel-ticket" role="tooltip"
+                                                    <div id="tooltip-cancel-ticket-{{ $ticket->id }}" role="tooltip"
                                                         class="tooltip invisible absolute z-10 inline-block rounded-lg bg-red-500 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300">
                                                         Cancelar ticket
                                                         <div class="tooltip-arrow" data-popper-arrow></div>
@@ -139,7 +138,7 @@
                             <div class="flex flex-col gap-2 rounded-xl border border-zinc-200 bg-white p-4 shadow-sm">
                                 <div class="flex items-center justify-between">
                                     <span
-                                        class="font-secondary flex items-center gap-2 rounded-xl bg-blue-100 px-2 py-1 text-lg font-bold text-secondary">
+                                        class="font-secondary text-secondary flex items-center gap-2 rounded-xl bg-blue-100 px-2 py-1 text-lg font-bold">
                                         <x-icon-store icon="ticket-02" class="h-6 w-6 text-blue-500" />
                                         {{ $ticket->ticket_number }}
                                     </span>
@@ -225,7 +224,7 @@
                     </div>
                 @else
                     <div
-                        class="mt-3 flex items-center justify-center gap-2 rounded-xl border-2 border-dashed border-zinc-300 bg-zinc-100 p-20 text-sm">
+                        class="mt-3 flex items-center justify-center gap-2 rounded-xl border-2 border-dashed border-zinc-300 p-20 text-sm">
                         <x-icon-store icon="information-circle" class="h-6 w-6 text-zinc-500" />
                         <p class="text-center text-zinc-800">
                             No tienes tickets de soporte técnico
@@ -248,11 +247,11 @@
                             <x-icon-store icon="alert" class="h-6 w-6 text-red-600" />
                         </div>
                         <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                            <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-headline">
+                            <h3 class="text-lg font-medium leading-6 text-red-500" id="modal-headline">
                                 Cancelar ticket
                             </h3>
                             <div class="mt-2">
-                                <p class="text-sm text-gray-500">
+                                <p class="font-dine-r text-sm text-zinc-400">
                                     ¿Estás seguro de que deseas cancelar este ticket?
                                 </p>
                             </div>
