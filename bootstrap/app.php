a6446094d5151a8ed88574cc18ac512e8c592f6d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-26 21:32:58 -0600

    feat: add api routes and sanctum config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,15 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        api: __DIR__ . '/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(["role" => App\Http\Middleware\Role::class]);
         $middleware->appendToGroup('web', \App\Http\Middleware\SetLocale::class);
         $middleware->appendToGroup("web", \App\Http\Middleware\CurrencyCurrent::class);
+        $middleware->validateCsrfTokens(except: [
+            '/billing/wompi'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
