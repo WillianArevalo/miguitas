eecfad7749b442f2a8ee4906e2e5d6ed872077d0
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-27 14:56:59 -0600

    styles: update login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,54 +1,65 @@
-@extends('layouts.template')
+@extends('layouts.login-template')
 @section('title', 'Miguitas | Inicio de sesión')
 @section('content')
-    <div class="m-0 md:p-4 lg:mx-10 lg:mb-10">
-        <div class="relative mx-auto flex h-full w-full flex-col overflow-hidden border md:flex-row xl:w-4/5">
+    <div class="flex">
+        <div class="relative flex h-screen w-full flex-1 items-start justify-center">
+            <img alt="login-logo" src="{{ asset('img/logo.png') }}" class="size-40 absolute top-10">
+            <img src="{{ asset('img/login-3.jpg') }}" alt="Login background" class="h-full w-full object-cover">
+        </div>
+        <div class="mx-auto flex flex-1 flex-col items-center justify-center bg-blue-store">
+            <h1 class="font-dine-r text-xl font-bold text-light-gray sm:text-2xl md:text-4xl">Inicio de sesión</h1>
 
-            <div class="absolute left-5 top-4 h-2 w-full bg-white md:left-10 md:top-8"></div>
-            <div class="absolute bottom-4 right-5 h-2 w-full bg-white md:bottom-8 md:right-10"></div>
-            <div class="absolute left-5 top-4 h-full w-2 bg-white md:left-10 md:top-8"></div>
-            <div class="absolute bottom-4 right-5 h-full w-2 bg-white md:bottom-8 md:right-10"></div>
-
-            <div class="flex flex-1 flex-col items-center justify-center gap-2 bg-light-pink py-10 md:pe-10 md:ps-20">
-                <h2 class="pt-10 text-center text-xl font-semibold text-dark-pink md:pt-0 md:text-2xl">
+            <div class="flex flex-col items-center justify-center">
+                <h2 class="mt-2 font-dine-r text-base font-bold text-pink-store sm:text-lg md:text-xl">
                     ¡Bienvenido de nuevo!
                 </h2>
-                <p class="w-1/2 text-center text-sm text-dark-blue md:w-full md:text-base">
-                    Bienvenido al mundo donde se cumplen los deseos de tu mejor amigo peludo
+                <p class="mt-2 w-2/3 text-center font-dine-r text-xs text-light-gray sm:text-sm md:text-base">
+                    Bienvenido al mundo donde se cumplen los deseos de tu mejor amigo peludo.
                 </p>
             </div>
-            <div class="flex flex-[2] flex-col items-center justify-center bg-light-blue py-20 md:pe-10">
-                <h1 class="font-dine-r text-xl font-bold text-light-gray sm:text-2xl md:text-4xl">Inicio de sesión</h1>
-                <div class="my-4 flex items-center justify-center gap-4">
-                    <a href="#">
-                        <x-icon-store icon="facebook" class="h-8 w-8 text-white" />
-                    </a>
-                    <a href="{{ Route('auth.google') }}">
-                        <x-icon-store icon="google" class="h-8 w-8 text-white" />
-                    </a>
-                    <a href="#">
-                        <x-icon-store icon="linkedin" class="h-8 w-8 text-white" />
-                    </a>
-                </div>
-                <div class="mt-4 w-4/5 md:w-2/3">
-                    <form action="{{ Route('login.validate') }}" method="POST">
-                        @csrf
-                        <div class="flex flex-col gap-2">
-                            <x-input-store type="email" name="email" placeholder="Correo electrónico" icon="email" />
-                        </div>
-                        <div class="flex flex-col gap-2">
-                            <x-input-store type="password" name="password" placeholder="Contraseña" icon="key" />
-                        </div>
-                        <div class="mt-4 flex flex-col items-center justify-center gap-4">
+
+            <div class="mt-4 w-4/5 md:w-2/3">
+                <form action="{{ Route('login.validate') }}" method="POST">
+                    @csrf
+                    <div class="flex flex-col">
+                        <x-input-store type="email" name="email" label="Correo electrónico"
+                            placeholder="Correo electrónico" icon="email" class="text-white" />
+                    </div>
+                    <div class="mt-2 flex flex-col">
+                        <x-input-store type="password" label="Contraseña" class="text-white" name="password"
+                            placeholder="Contraseña" icon="key" />
+                    </div>
+                    <div class="mt-4 flex flex-col items-center justify-center gap-4">
+                        <div class="flex w-full justify-between">
+                            <div class="flex items-center gap-2">
+                                <input id="remember" type="checkbox"
+                                    class="rounded-sm border-pink-store text-light-blue focus:ring-0" name="remember">
+                                <label class="text-sm text-white" for="remember">
+                                    Recuerdame
+                                </label>
+                            </div>
                             <a href="#" class="font-dine-r text-white underline">¿Olvidaste tu contraseña?</a>
-                            <x-button-store type="submit" text="Iniciar sesión" icon="login" typeButton="primary" />
-                            <p class="font-pluto-r text-white">
-                                ¿No tienes una cuenta? <a href="{{ Route('register') }}"
-                                    class="font-dine-r text-white underline">Regístrate</a>
-                            </p>
                         </div>
-                    </form>
-                </div>
+                        <x-button-store type="submit" text="Iniciar sesión" icon="login" typeButton="tertiary"
+                            class="mt-4" />
+                        <div>
+                            <p class="text-center font-dine-r text-white">
+                                O inicia sesión con
+                            </p>
+                            <div class="mt-4 flex flex-col justify-center gap-4">
+                                <a href="{{ Route('auth.google') }}"
+                                    class="flex items-center justify-center gap-2 rounded-xl bg-white px-6 py-2 font-pluto-r text-sm text-zinc-500 hover:bg-zinc-200">
+                                    <x-icon-store icon="google" class="size-5 text-current" />
+                                    Iniciar sesión con Google
+                                </a>
+                            </div>
+                        </div>
+                        <p class="font-pluto-r text-white">
+                            ¿No tienes una cuenta? <a href="{{ Route('register') }}"
+                                class="font-dine-r text-white underline">Regístrate</a>
+                        </p>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
