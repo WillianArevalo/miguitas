195d89da7b8af622f22b551fa986611ad228ed3f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-27 14:58:15 -0600

    styles: update sidebar profile

--- resources/views/layouts/__partials/store/template-profile.blade.php
+++ resources/views/layouts/__partials/store/template-profile.blade.php
@@ -7,7 +7,7 @@
     <div>
         <section class="mx-auto p-0 xl:px-8">
             <div class="mx-auto flex flex-col px-4 sm:mt-8 md:gap-4 xl:flex-row xl:gap-8">
-                <div class="mx-auto mb-4 h-max w-max overflow-hidden sm:mx-0 sm:w-auto sm:overflow-auto xl:min-w-[250px]">
+                <div class="mx-auto mb-4 h-max w-max overflow-hidden sm:mx-0 sm:w-auto sm:overflow-auto xl:min-w-[350px]">
                     @include('layouts.__partials.store.nav-profile')
                 </div>
                 <div class="h-max w-full">
