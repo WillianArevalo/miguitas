62df9a692c0edb0a9887dc1f2db0cb7f5f800886
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-27 14:57:27 -0600

    feat: update login template

--- resources/views/layouts/login-template.blade.php
+++ resources/views/layouts/login-template.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" href="{{ asset('images/imagen6.png') }}" type="image/x-icon">
-    @vite('resources/css/app.css')
+    <link rel="shortcut icon" href="{{ $favicon ? Storage::url($favicon) : asset('images/logo.png') }}">
+    @vite('resources/css/store.css')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
         (function() {
             let theme = localStorage.getItem('theme') || '{{ App\Enums\Theme::LIGHT }}';
