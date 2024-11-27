9d6e1b5b99abe1889938c323ce0dab3c0cb46995
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-26 21:36:41 -0600

    feat: add view email verified

---
+++ resources/views/store/email/email-verified.blade.php
@@ -0,0 +1,12 @@
+@extends('store.login-template')
+@section('title', 'Miguitas | Verifica tu correo electrónico')
+@section('content')
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-center text-2xl font-bold">
+            Verifica tu correo electrónico
+        </h1>
+        <p class="text-center text-zinc-500">
+            Se ha enviado un correo electrónico para verificar tu cuenta.
+        </p>
+    </div>
+@endsection
