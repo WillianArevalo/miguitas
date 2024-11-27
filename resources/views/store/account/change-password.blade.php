29cd660a39ecb12bc3e15aa28b7cec14b5e2e63c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-26 21:36:34 -0600

    styles: update change password

--- resources/views/store/account/change-password.blade.php
+++ resources/views/store/account/change-password.blade.php
@@ -1,31 +1,25 @@
-@extends('layouts.login-template')
+@extends('layouts.template')
 @section('title', 'Cambiar contraseña')
 @section('content')
-    <div class="relative flex h-screen items-center justify-center"
-        style="background-image:url('{{ asset('images/fondo6.jpg') }}'); background-position:center; background-repeat: no-repeat; background-size: cover;">
-        <section class="z-10 w-4/12 rounded-3xl bg-white p-4 py-10 shadow-lg">
+    <div class="flex items-center justify-center py-8">
+        <section class="z-10 w-4/12 rounded-3xl bg-white p-4 py-10">
             <div class="text-center">
-                <h1 class="text-2xl font-bold text-secondary">Cambiar contraseña</h1>
+                <h1 class="font-pluto-r text-2xl font-bold text-blue-store">Cambiar contraseña</h1>
             </div>
             <form action="{{ Route('account.edit-password') }}" method="POST" class="mx-auto mt-4 flex w-3/4 flex-col gap-4"
                 id="formChangePassword">
                 @csrf
                 <div class="flex flex-col gap-2">
                     <x-input-store type="password" id="password" name="password" label="Contraseña actual"
-                        placeholder="Ingresa tu contraseña actual" class="text-zinc-800" icon="password" />
+                        placeholder="Ingresa tu contraseña actual" class="text-zinc-800" icon="key" />
                 </div>
                 <div class="flex flex-col gap-2">
                     <x-input-store type="password" id="new-password" name="new-password" label="Nueva contraseña"
-                        placeholder="Ingresa la nueva contraseña" class="text-zinc-800" icon="password" />
+                        placeholder="Ingresa la nueva contraseña" class="text-zinc-800" icon="key" />
                 </div>
                 <div class="flex flex-col gap-2">
                     <x-input-store type="password" id="confirm-password" name="confirm-password" label="Confirma contraseña"
-                        icon="password-validation" placeholder="Confirma la contraseña" class="text-zinc-800" />
-                </div>
-                <div class="my-2 flex items-center justify-center">
-                    <a href="" class="text-sm font-semibold text-blue-500 underline hover:text-blue-700">
-                        ¿Olvidaste tu contraseña?
-                    </a>
+                        icon="key" placeholder="Confirma la contraseña" class="text-zinc-800" />
                 </div>
                 <div class="flex items-center justify-center">
                     <x-button-store typeButton="primary" id="btn-change-password" type="button" text="Cambiar contraseña"
@@ -33,4 +27,5 @@
                 </div>
             </form>
         </section>
-    @endsection
+    </div>
+@endsection
