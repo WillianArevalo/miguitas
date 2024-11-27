59cdbf209e85357e31b4642e2dd082d7536cef16
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-26 21:36:29 -0600

    feat: update template profile

--- resources/views/layouts/__partials/store/template-profile.blade.php
+++ resources/views/layouts/__partials/store/template-profile.blade.php
@@ -6,8 +6,8 @@
     @endphp
     <div>
         <section class="mx-auto p-0 xl:px-8">
-            <div class="mx-auto mt-4 flex flex-col px-4 sm:mt-8 sm:flex-row md:gap-4 xl:gap-8 xl:px-20">
-                <div class="mx-auto mb-4 h-max w-max overflow-hidden sm:mx-0 sm:w-auto sm:overflow-auto xl:min-w-[350px]">
+            <div class="mx-auto flex flex-col px-4 sm:mt-8 md:gap-4 xl:flex-row xl:gap-8">
+                <div class="mx-auto mb-4 h-max w-max overflow-hidden sm:mx-0 sm:w-auto sm:overflow-auto xl:min-w-[250px]">
                     @include('layouts.__partials.store.nav-profile')
                 </div>
                 <div class="h-max w-full">
