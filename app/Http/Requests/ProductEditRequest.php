60228300bd4735988e737161d69e5f6e8fa97d07
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-28 17:02:09 -0600

    feat: update product request

--- app/Http/Requests/ProductEditRequest.php
+++ app/Http/Requests/ProductEditRequest.php
@@ -32,13 +32,14 @@ class ProductEditRequest extends FormRequest
             "offer_start_date" => "nullable|date",
             "offer_end_date" => "nullable|date",
             "offer_active" => "nullable|boolean",
-            "sku" => "required|string",
-            "stock" => "required|numeric",
-            "barcode" => "required|string",
+            "sku" => "nullable|string",
+            "stock" => "nullable|numeric",
+            "barcode" => "nullable|string",
             "weight" => "nullable|numeric",
             "categorie_id" => "required|numeric",
             "is_active" => "nullable|boolean",
             "is_top" => "nullable|boolean",
+            "subcategories" => "nullable|array",
         ];
     }
 }
