072c8bc8491ecc83d7e9f400e8eae0c110aace3a
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-28 19:00:32 -0600

    styles: update product view

--- resources/views/store/products/view.blade.php
+++ resources/views/store/products/view.blade.php
@@ -57,7 +57,6 @@
                                     @endif
                                 @endfor
                             </div>
-
                         </div>
                     </div>
                     <div>
