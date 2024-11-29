3cd6ed1c3e3e042bdba02e49b9190e13ba0d3b7d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-28 19:00:18 -0600

    feat: add logic to send contact messages

--- resources/views/store/contact/index.blade.php
+++ resources/views/store/contact/index.blade.php
@@ -2,39 +2,47 @@
 @section('title', 'Miguitas | Contactanos')
 @section('content')
     <div>
-        <div class="mx-auto flex w-full items-center justify-center md:w-4/5 lg:w-3/4">
+        <div class="mx-auto flex w-full items-center justify-center md:w-4/5 lg:w-3/4" data-aos="fade-up">
             <img src="{{ asset('img/contactanos.png') }}" alt="Shop image" class="h-full w-full rounded-3xl">
         </div>
         <div class="mt-10 flex flex-col items-center justify-center gap-4">
-            <h1 class="light-blue-2 font-weight-bold text-4xl">CONTÁCTANOS</h1>
+            <h1 class="font-pluto-m text-4xl font-bold text-blue-store">CONTÁCTANOS</h1>
             <div class="flex flex-col items-center justify-center gap-4 px-4">
                 <div class="flex items-center justify-center gap-2 text-center">
-                    <x-icon-store icon="location-arrow" class="h-6 w-6 fill-light-blue" />
-                    <h2 class="light-blue font-pluto-r text-sm sm:text-base md:text-lg">
-                        Antiguo Cuscatlán, Col Cumbres de Cuscatlán, La Libertad, El Salvador.
+                    <span class="size-12 flex items-center justify-center rounded-full bg-purple-100">
+                        <x-icon-store icon="location-arrow" class="h-6 w-6 text-blue-store" />
+                    </span>
+                    <h2 class="light-blue w-1/2 font-pluto-r text-sm sm:text-base md:text-lg">
+                        {{ $settings->where('key', 'store_address')->first()->value }}
                     </h2>
                 </div>
                 <div class="flex items-center justify-center gap-2">
-                    <x-icon-store icon="phone" class="h-6 w-6 fill-light-blue" />
+                    <span class="size-12 flex items-center justify-center rounded-full bg-purple-100">
+                        <x-icon-store icon="phone" class="h-6 w-6 text-blue-store" />
+                    </span>
                     <h2 class="light-blue font-pluto-r text-sm sm:text-base md:text-lg">
-                        (+503) 2243-4190
+                        {{ $settings->where('key', 'store_phone')->first()->value }}
                     </h2>
                 </div>
                 <div class="flex items-center justify-center gap-2">
-                    <x-icon-store icon="whatsapp" class="h-6 w-6 fill-light-blue" />
+                    <span class="size-12 flex items-center justify-center rounded-full bg-purple-100">
+                        <x-icon-store icon="whatsapp" class="h-6 w-6 text-blue-store" />
+                    </span>
                     <h2 class="light-blue font-pluto-r text-sm sm:text-base md:text-lg">
-                        (+503) 7910-1241
+                        {{ $settings->where('key', 'store_whatsapp')->first()->value }}
                     </h2>
                 </div>
                 <div class="flex items-center justify-center gap-2">
-                    <x-icon-store icon="email" class="h-6 w-6 fill-light-blue" />
+                    <span class="size-12 flex items-center justify-center rounded-full bg-purple-100">
+                        <x-icon-store icon="email" class="h-6 w-6 text-blue-store" />
+                    </span>
                     <h2 class="light-blue font-pluto-r text-sm sm:text-base md:text-lg">
-                        administración@miguitasssv.com
+                        {{ $settings->where('key', 'store_email')->first()->value }}
                     </h2>
                 </div>
             </div>
             <div class="my-10">
-                <h2 class="light-blue-2 text-center text-3xl">Horarios</h2>
+                <h2 class="text-center text-3xl font-semibold text-blue-store">Horarios</h2>
                 <ul class="ms-4 mt-4 flex list-disc flex-col gap-4 px-8 text-base md:text-lg">
                     <li class="font-din-r text-zinc-500">
                         Atención en REDES: Martes a Viernes 9:00 am a 6 pm y Sábados 9:00 am a 4:00 pm
@@ -56,27 +64,32 @@
                 </ul>
             </div>
             <div class="my-10 w-full px-4 md:w-1/2">
-                <h2 class="light-blue-2 font-weight-bold text-center text-xl sm:text-2xl md:text-3xl">ESCRÍBENOS</h2>
-                <form action="" class="mt-4 w-full" method="POST">
+                <h2 class="text-center text-xl font-bold text-blue-store sm:text-2xl md:text-3xl">ESCRÍBENOS</h2>
+                <form action="{{ route('contact.store') }}" class="mt-4 w-full" method="POST">
                     @csrf
                     <div class="flex flex-col items-center gap-2 sm:flex-row">
-                        <div class="flex w-full flex-1 flex-col gap-2">
-                            <x-input-store type="text" name="name" placeholder="Nombre" />
+                        <div class="flex w-full flex-1 flex-col gap-1">
+                            <x-input-store type="text" name="name" required label="Nombre"
+                                placeholder="Ingresa tu nombre" />
                         </div>
-                        <div class="flex w-full flex-1 flex-col gap-2">
-                            <x-input-store type="text" name="last_name" placeholder="Apellido" />
-                        </div>
-                    </div>
-                    <div class="flex flex-col items-center gap-2 sm:flex-row">
-                        <div class="flex w-full flex-1 flex-col gap-2 md:flex-[2]">
-                            <x-input-store type="email" icon="email" name="email" placeholder="Correo electrónico" />
-                        </div>
-                        <div class="flex w-full flex-1 flex-col gap-2">
-                            <x-input-store type="text" name="phone" icon="phone" placeholder="Telefono" />
+                        <div class="flex w-full flex-1 flex-col gap-1">
+                            <x-input-store type="text" name="last_name" label="Apellido"
+                                placeholder="Ingresa tu apellido" />
                         </div>
                     </div>
-                    <div class="flex flex-col items-center gap-2">
-                        <x-input-store type="textarea" name="message" placeholder="Mensaje" />
+                    <div class="mt-4 flex flex-col items-center gap-2 sm:flex-row">
+                        <div class="flex w-full flex-1 flex-col gap-1 md:flex-[2]">
+                            <x-input-store type="email" icon="email" name="email" label="Correo electrónico"
+                                placeholder="Ingresa tu correo electrónico" required />
+                        </div>
+                        <div class="flex w-full flex-1 flex-col gap-1">
+                            <x-input-store type="text" name="phone" icon="phone" label="Teléfono"
+                                placeholder="Ingresa tu teléfono" />
+                        </div>
+                    </div>
+                    <div class="mt-4 flex flex-col items-start gap-2">
+                        <x-input-store type="textarea" name="message" required label="Mensaje"
+                            placeholder="Ingresa tu mensaje..." />
                     </div>
                     <div class="mt-4 flex items-center justify-center">
                         <x-button-store type="submit" class="w-max px-10" typeButton="primary" text="Enviar" />
