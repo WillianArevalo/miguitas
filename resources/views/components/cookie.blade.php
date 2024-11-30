5570a10267c6a55b454460210cde76796894136f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-30 14:34:15 -0600

    feat: update cookie component

--- resources/views/components/cookie.blade.php
+++ resources/views/components/cookie.blade.php
@@ -1,22 +1,27 @@
 <section
-    class="cookies fixed bottom-16 left-12 z-50 mx-auto hidden max-w-md rounded-2xl border border-zinc-400 bg-white p-4">
-    <h2 class="text-lg font-semibold text-secondary">
+    class="cookies fixed bottom-16 left-12 z-50 mx-auto max-w-md animate-fade-right rounded-2xl border border-white bg-white p-4 shadow-lg">
+    <h2 class="text-lg font-semibold text-blue-store">
         Cookies
     </h2>
-    <p class="mt-2 text-xs text-zinc-600">
+    <p class="mt-2 font-dine-r text-xs text-zinc-600">
         Hola, este sitio web utiliza cookies esenciales para garantizar su correcto funcionamiento
-        Cookies de funcionamiento y seguimiento para comprender cómo interactúa con él. Este último se establecerá solo
+        y seguimiento para comprender cómo interactúa con él. Este último se establecerá solo
         después de consentimiento.
-        <a href="#" class="font-medium text-zinc-700 underline transition-colors duration-300 hover:text-blue-600">
+        <a href="#"
+            class="font-dine-b font-medium text-zinc-700 underline transition-colors duration-300 hover:text-blue-600">
             Política de cookies
         </a>.
     </p>
-    <p class="mt-3 text-xs text-zinc-600">
+    <p class="mt-3 font-dine-r text-xs text-zinc-600">
         Al cerrar esta configuración modal, se guardará la configuración predeterminada.
     </p>
     <div class="mt-4 grid shrink-0 grid-cols-2 gap-4">
-        <x-button-store type="button" typeButton="secondary" text="Preferencias" class="text-xs" />
-        <x-button-store type="button" typeButton="primary" text="Aceptar todas" class="text-xs" />
+        <x-button-store type="button" typeButton="secondary" size="small" text="Preferencias" class="text-xs" />
+        <form action="{{ Route('accept-all-cookies') }}" method="POST">
+            @csrf
+            <x-button-store type="button" typeButton="primary" size="small" id="accept-all-cookies"
+                text="Aceptar todas" class="text-xs" />
+        </form>
     </div>
     <button class="close-cookies absolute right-0 top-0 m-4">
         <x-icon icon="x" class="h-4 w-4 text-zinc-600" />
