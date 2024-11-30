252d02552792fbb1cd4dec5240d140b5338b3af5
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-30 14:34:25 -0600

    styles: update 404 page view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,7 +2,9 @@
 @section('title', 'Página no encontrada')
 @section('content')
     <div class="relative h-screen w-full">
-        <img src="{{ asset('img/404.png') }}" alt="Image page not found" class="h-full w-full">
-        <a href="{{ Route('home') }}" class="absolute bottom-0 right-0 m-10 font-bold text-pink-600">Volver al inicio</a>
+        <img src="{{ asset('img/logo.png') }}" alt="Logo" class="size-28 absolute left-0 top-0 m-10">
+        <img src="{{ asset('img/404.png') }}" alt="Image page not found" class="h-full w-full object-cover">
+        <x-button-store href="{{ Route('home') }}" typeButton="primary" class="absolute bottom-0 right-0 m-10"
+            text="Volver al inicio" size="large" />
     </div>
 @endsection
