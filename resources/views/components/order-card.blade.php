5ce97bd58832751ac5cf6e951a178a67b30bb509
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-02 13:56:36 -0600

    feat: update order card styles and icons for improved UI consistency

--- resources/views/components/order-card.blade.php
+++ resources/views/components/order-card.blade.php
@@ -1,12 +1,12 @@
 <div class="{{ $styles }} flex flex-col gap-2 overflow-hidden rounded-xl border p-4 shadow">
     <div class="flex justify-between">
-        <h3 class="flex items-center gap-1 text-lg font-bold text-secondary sm:text-xl">
+        <h3 class="flex items-center gap-1 text-lg font-bold text-blue-store sm:text-xl">
             <x-icon-store icon="shopping-bag" class="h-5 w-5 text-current" />
             {{ $order->number_order }}
         </h3>
         <span
-            class="{{ $statusBorder }} ml-auto flex items-center gap-1 rounded-xl border px-2 py-1 text-xs font-medium text-white">
-            <x-icon-store :icon="$statusIcon" class="h-4 w-4 text-white" />
+            class="{{ $statusBorder }} ml-auto flex items-center gap-1 rounded-xl border px-2 py-1 text-xs font-medium">
+            <x-icon-store :icon="$statusIcon" class="h-4 w-4 text-current" />
             {{ ucfirst($order->status) }}
         </span>
     </div>
@@ -24,17 +24,17 @@
     <div class="flex items-center justify-between gap-2">
         <div class="flex flex-col">
             <span class="text-sm text-zinc-600">Total:</span>
-            <span class="text-lg font-bold text-secondary">${{ $order->total }}</span>
+            <span class="text-lg font-bold text-blue-store">${{ $order->total }}</span>
         </div>
         <div class="flex items-center gap-2">
-            <x-button-store icon="view" type="a" href="{{ Route('orders.show', $order->number_order) }}"
+            <x-button-store icon="eye" type="a" href="{{ Route('orders.show', $order->number_order) }}"
                 typeButton="secondary" onlyIcon="true" class="w-max" />
             @if ($order->status !== 'completed' && $order->status !== 'canceled' && $order->status !== 'sent')
                 <div>
                     <form action="{{ Route('order.cancel', $order->id) }}" method="POST"
                         id="formCancelOrder-{{ $order->id }}">
                         @csrf
-                        <x-button-store icon="cancel-circle" type="button"
+                        <x-button-store icon="delete" type="button"
                             href="{{ Route('account.tickets.show', $order->id) }}" typeButton="danger" onlyIcon="true"
                             class="buttonDelete w-max" data-tooltip-target="tooltip-cancel-ticket"
                             data-form="formCancelOrder-{{ $order->id }}" />
