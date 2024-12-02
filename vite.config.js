92c77d2a58c6593ce538d1b1d4475d78527c840e
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-02 13:14:03 -0600

    feat: add admin.css to Vite configuration for improved styling

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/css/store.css",
+                "resources/css/admin.css",
                 "resources/css/store/galeria.css",
                 "resources/js/app.js",
 
