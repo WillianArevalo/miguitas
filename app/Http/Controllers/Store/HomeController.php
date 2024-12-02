c193eac7eccf40f4f5f23f7112dc1a2ebfae7c8b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-02 09:22:40 -0600

    feat: add popup management functionality to handle active popups and user interactions

--- app/Http/Controllers/Store/HomeController.php
+++ app/Http/Controllers/Store/HomeController.php
@@ -6,10 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Helpers\Favorites;
 use App\Models\Categorie;
 use App\Models\HeadBand;
+use App\Models\Popup;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -32,6 +33,15 @@ class HomeController extends Controller
                 ->where('is_active', true);
         }])->get();
 
+        $referencesIdsPopups = Cookie::get("popup");
+        $referencesIds = json_decode($referencesIdsPopups, true);
+        $popups = Popup::where("active", true)
+            ->when(!empty($referencesIds), function ($query) use ($referencesIds) {
+                $query->whereNotIn("reference_id", $referencesIds);
+            })
+            ->take(1)
+            ->get();
+
         Favorites::get($user, $products);
         Favorites::get($user, $topProducts);
 
@@ -41,6 +51,7 @@ class HomeController extends Controller
             "flashOffers" => $flashOffers,
             "categories" => $categories,
             "theMonthProducts" => $theMonthProducts,
+            "popups" => $popups
         ]);
     }
 
@@ -58,5 +69,32 @@ class HomeController extends Controller
     public function showCookies()
     {
         return view("store.policy-cookies");
+    }
+
+    public function showPopup(Request $request)
+    {
+        $reference = $request->input("reference_id");
+        $existingPopups = json_decode(Cookie::get("popup", "[]", true));
+
+        if (!in_array($reference, $existingPopups)) {
+            $existingPopups[] = $reference;
+        }
+
+        $popup = Popup::where("reference_id", $reference)->first();
+        if ($popup) {
+            Cookie::queue(
+                'popup',
+                json_encode($existingPopups), // Convertir a JSON
+                60 * 24 * 365,               // Duración: 1 año
+                '/',
+                null,
+                true,                        // HTTPS
+                true,                        // HttpOnly
+                'lax'
+            );
+            return response()->json(["success" => "Popup visto"]);
+        } else {
+            return response()->json(["error" => "Popup no encontrado"], 404);
+        }
     }
 }
