2efd30db350418e75ee4ee8a8e17f090aea23e03
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-10 20:44:58 -0600

    feat: Add image preview and removal functionality for ticket attachments

--- resources/js/admin/ticket.js
+++ resources/js/admin/ticket.js
@@ -7,4 +7,38 @@ $(document).ready(function () {
         );
         form.submit();
     });
+
+    const $previewImages = $("#preview-images");
+    const $inputImages = $("#attachments");
+    const $btnRemoveImages = $("#btn-remove-attachments");
+
+    $inputImages.on("change", function () {
+        $("#container-preview-images").removeClass("hidden");
+        $previewImages.html("");
+        const files = Array.from($inputImages.prop("files"));
+        $btnRemoveImages.removeClass("hidden");
+        files.forEach((file) => {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                $previewImages.append(
+                    `<img src="${e.target.result}" class="rounded-lg size-32 object-cover" width="100">`
+                );
+            };
+            reader.readAsDataURL(file);
+        });
+    });
+
+    $btnRemoveImages.on("click", function () {
+        $inputImages.val("");
+        $previewImages.html("");
+        $btnRemoveImages.addClass("hidden");
+        $("#container-preview-images").addClass("hidden");
+    });
+
+    $(".change-status-ticket").on("click", function () {
+        let status = $(this).data("status");
+        const form = $(this).closest("form");
+        form.find("input[name=status]").val(status);
+        form.submit();
+    });
 });
