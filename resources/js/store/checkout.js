53100ff833bfbcd2d86133f9c14238c0792d6147
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-10 20:45:04 -0600

    feat: Add dynamic loading of municipios and distritos based on selected state and municipio

--- resources/js/store/checkout.js
+++ resources/js/store/checkout.js
@@ -112,4 +112,64 @@ $(document).ready(function () {
             });
         }
     );
+
+    $("#state").on("Changed", function () {
+        const value = $(this).val();
+        const url = $(this).data("url");
+        $(".itemSelectedMunicipio").html("Seleccione un municipio");
+        $(".itemSelectedDistrito").html("Seleccione un distrito");
+        $.ajax({
+            url: url,
+            method: "GET",
+            data: { state: value },
+            success: function (response) {
+                if (response.status === "success") {
+                    $("#list-municipios").html(response.html);
+                }
+                console.log(response);
+            },
+            error: function (response) {
+                if (response.status === "error") {
+                    showToast(response.message, "error");
+                    console.log(response);
+                }
+                console.log(response);
+            },
+        });
+    });
+
+    $(document).on("click", ".itemOptionMunicipio", function () {
+        $(".itemSelectedMunicipio").html($(this).html());
+        $("#municipio").val($(this).data("value")).trigger("Changed");
+    });
+
+    $(document).on("click", ".itemOptionDistrito", function () {
+        $(".itemSelectedDistrito").html($(this).html());
+        $("#distrito").val($(this).data("value")).trigger("Changed");
+    });
+
+    $("#municipio").on("Changed", function () {
+        const value = $(this).val();
+        const url = $(this).data("url");
+        const state = $("#state").val();
+        $(".itemSelectedDistrito").html("Seleccione un distrito");
+        $.ajax({
+            url: url,
+            method: "GET",
+            data: { state: state, city: value },
+            success: function (response) {
+                if (response.status === "success") {
+                    $("#list-distritos").html(response.html);
+                }
+                console.log(response);
+            },
+            error: function (response) {
+                if (response.status === "error") {
+                    showToast(response.message, "error");
+                    console.log(response);
+                }
+                console.log(response);
+            },
+        });
+    });
 });
