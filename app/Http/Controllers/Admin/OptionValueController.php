1f8bb9a4b0ceee9b264992b7319a54c782ca4c85
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-13 13:50:52 -0600

    feat: Improve product options management by enabling the creation and assignment of existing values

--- app/Http/Controllers/Admin/OptionValueController.php
+++ app/Http/Controllers/Admin/OptionValueController.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use App\Models\ProductOption;
 use App\Models\ProductOptionValue;
 use Illuminate\Support\Facades\DB;
+use phpseclib3\Crypt\RC2;
 
 class OptionValueController extends Controller
 {
@@ -17,42 +18,58 @@ class OptionValueController extends Controller
             "option_id" => "required|integer",
             "product_id" => "required|integer",
             "value" => "required|string",
-            "price" => "required|numeric",
-            "stock" => "required|integer"
         ]);
+
         DB::beginTransaction();
         try {
-            $option = ProductOption::findOrFail($request->input("option_id"));
             $product = Product::findOrFail($request->input("product_id"));
-            $value =
-                ProductOptionValue::create([
-                    "product_option_id" => $option->id,
-                    "value" => $request->input("value")
-                ]);
+            $optionParent = ProductOption::findOrFail($request->input("option_id"));
 
-            if (!$value) {
-                return back()->with("error", "No se pudo crear la opción");
+            if (!$product || !$optionParent) {
+                return back()->with("error", "No se encontró el producto o la opción.");
             }
 
-            $product->options()->attach($value->id, [
-                "price" => $request->input("price"),
-                "stock" => $request->input("stock"),
+            $existingOptionValue = ProductOptionValue::where("product_option_id", $optionParent->id)
+                ->where("value", $request->input("value"))
+                ->first();
+
+            if ($existingOptionValue) {
+                if (!$product->options()->where("product_option_value_id", $existingOptionValue->id)->exists()) {
+                    $product->options()->attach($existingOptionValue->id);
+                }
+                DB::commit();
+                return redirect()->route("admin.products.edit", $product->id)->with("success", "Opción asignada correctamente.");
+            }
+
+            $newOptionValue = ProductOptionValue::create([
+                "product_option_id" => $optionParent->id,
+                "value" => $request->input("value"),
             ]);
 
+            if (!$newOptionValue) {
+                DB::rollBack();
+                return back()->with("error", "No se pudo crear la opción.");
+            }
+
+            $product->options()->attach($newOptionValue->id);
             DB::commit();
-            return back()->with("success", "Opción creada correctamente.");
+            return redirect()->route("admin.products.edit", $product->id)->with("success", "Opción creada correctamente.");
         } catch (\Exception $e) {
             DB::rollBack();
             return back()->with("error", "Error: " . $e->getMessage());
-            dd($e);
         }
     }
 
-    public function destroy(string $id)
+    public function destroy(string $id, Request $request)
     {
         DB::beginTransaction();
         try {
-            ProductOptionValue::destroy($id);
+            $product = Product::findOrFail($request->input("product_id"));
+            if (!$product) {
+                return back()->with("error", "No se encontró el producto.");
+            }
+
+            $product->options()->detach($id);
             DB::commit();
             return back()->with("success", "Opción eliminada correctamente.");
         } catch (\Exception $e) {
