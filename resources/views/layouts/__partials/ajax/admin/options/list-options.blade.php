8a4b28849a172ffb5e2e36b1433c64d8ba84887c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-13 13:53:05 -0600

    feat: Refactor options list to use a more semantic structure with improved styling

--- resources/views/layouts/__partials/ajax/admin/options/list-options.blade.php
+++ resources/views/layouts/__partials/ajax/admin/options/list-options.blade.php
@@ -1,26 +1,16 @@
 @if ($options->count() > 0)
-    <div class="flex w-full flex-col gap-4">
+    <ul class="flex w-full flex-col">
         @foreach ($options as $option)
-            <div class="flex items-start justify-between">
-                <div class="flex flex-1 flex-col justify-center gap-2">
-                    <label for="{{ $option->name }}" class="flex items-center gap-2">
-                        <span class="text-sm text-zinc-500 dark:text-zinc-300">
-                            {{ $option->name }}
-                        </span>
-                    </label>
+            <li class="flex gap-2 rounded-lg px-4 py-2.5 text-sm text-zinc-900 hover:bg-zinc-100 dark:text-white dark:hover:bg-zinc-900"
+                data-value="{{ $option->id }}">
+                <div>
+                    <x-input type="checkbox" value="{{ $option->id }}" name="options_checkbox"
+                        data-name="{{ $option->value }}" data-option-id="{{ $optionParent->id }}" />
                 </div>
-                <div class="flex flex-1 items-center justify-end gap-2">
-                    <x-button type="button" typeButton="secondary" size="small" text="Agregar opciones" icon="plus"
-                        data-modal-target="addOptionValue" class="showModalOptionValue"
-                        data-modal-toggle="addOptionValue" data-container="#previewOptions-{{ $option->id }}"
-                        data-id="{{ $option->id }}" />
-                </div>
-            </div>
-            <div id="previewOptions-{{ $option->id }}" class="flex gap-2">
-            </div>
+                {{ $option->value }}
+            </li>
         @endforeach
-    </div>
-    <div id="hiddenOptionsContainer"></div>
+    </ul>
 @else
     <x-paragraph>
         Sin opciones registradas
