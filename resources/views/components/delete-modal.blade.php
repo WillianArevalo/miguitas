da7130b26f38e2d469458b8364e2b9347271560c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-13 13:52:49 -0600

    feat: Improves the delete modal with a new design and icon styles

--- resources/views/components/delete-modal.blade.php
+++ resources/views/components/delete-modal.blade.php
@@ -16,17 +16,19 @@
                       </svg>
                       <span class="sr-only">Close modal</span>
                   </button>
-                  <svg class="mx-auto mb-3.5 h-11 w-11 text-zinc-400 dark:text-zinc-500" aria-hidden="true"
-                      fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd"
-                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                          clip-rule="evenodd"></path>
-                  </svg>
+                  <span
+                      class="mx-auto mb-4 flex w-max items-center justify-center rounded-full bg-red-100 p-3 text-red-500 dark:bg-red-900/30">
+                      <svg class="size-11 mx-auto text-current" aria-hidden="true" fill="currentColor"
+                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                              clip-rule="evenodd"></path>
+                      </svg>
+                  </span>
                   <p class="mb-4 text-zinc-500 dark:text-zinc-300">{{ $title }}</p>
                   <p class="mb-6 text-sm text-zinc-500 dark:text-zinc-400">{{ $message }}</p>
               </div>
-              <div
-                  class="flex items-center justify-center space-x-4 border-t border-zinc-300 py-4 dark:border-zinc-900">
+              <div class="flex items-center justify-center space-x-4 py-4">
                   <x-button type="button" data-modal-toggle="{{ $modalId }}" class="closeModal"
                       text="No, cancelar" icon="cancel" typeButton="secondary" />
                   <x-button type="button" class="confirmDelete" text="Sí, eliminar" icon="delete"
