befed28fcd6aeb6f8386b4db3837a23279464641
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-13 22:25:32 -0600

    feat: Add department and district selection in the address form

--- resources/views/store/account/address/new.blade.php
+++ resources/views/store/account/address/new.blade.php
@@ -22,6 +22,64 @@
                                 required value="{{ old('type') }}" selected="{{ old('type') }}" />
                         </div>
                     </div>
+                    <div class="flex flex-col gap-4 sm:flex-row">
+                        <div class="flex w-full flex-1 flex-col gap-2">
+                            <x-select-store name="state" label="Departamento" id="state"
+                                value="{{ $address->state ?? '' }}" data-url="{{ Route('departamentos.search') }}"
+                                selected="{{ $address->state ?? '' }}" :options="$departamentos" />
+                        </div>
+                        <div class="w-full flex-1">
+                            <label class="mb-2 block text-start text-sm font-medium text-zinc-600 md:text-base">
+                                Municipio
+                            </label>
+                            <input type="hidden" id="municipio" name="municipio" value="{{ $address->city ?? '' }}"
+                                data-url="{{ Route('distritos') }}">
+                            <div class="relative">
+                                <div
+                                    class="selected @error('municipio') is-invalid @enderror flex w-full items-center justify-between rounded-xl border-2 border-blue-store bg-white px-6 py-3 text-sm text-zinc-700 md:text-base">
+                                    <span class="itemSelectedMunicipio truncate font-pluto-r" id="municipio_selected">
+                                        Seleccione un departamento
+                                    </span>
+                                    <x-icon icon="arrow-down" class="ms-4 h-5 w-5 text-zinc-500" />
+                                </div>
+                                <ul class="selectOptions absolute z-10 mb-8 mt-2 hidden h-auto w-full overflow-auto rounded-xl border-2 border-blue-store bg-white p-2 shadow-lg"
+                                    id="list-municipios">
+                                    <li class="itemOption cursor-default truncate rounded-xl px-4 py-2.5 font-pluto-r text-sm text-zinc-700 hover:bg-zinc-100 md:text-base"
+                                        data-input="#municipio">
+                                        Selecciona un departamento
+                                    </li>
+                                </ul>
+                            </div>
+                            @error('municipio')
+                                <span class="text-sm text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="w-full flex-1">
+                        <label class="mb-2 block text-start text-sm font-medium text-zinc-600 md:text-base">
+                            Distrito
+                        </label>
+                        <input type="hidden" id="distrito" name="distrito">
+                        <div class="relative">
+                            <div
+                                class="selected @error('distrito') is-invalid @enderror flex w-full items-center justify-between rounded-xl border-2 border-blue-store bg-white px-6 py-3 text-sm text-zinc-700 md:text-base">
+                                <span class="itemSelectedDistrito truncate font-pluto-r" id="municipio_selected">
+                                    Seleccione un distrito
+                                </span>
+                                <x-icon icon="arrow-down" class="ms-4 h-5 w-5 text-zinc-500" />
+                            </div>
+                            <ul class="selectOptions absolute z-10 mb-8 mt-2 hidden h-auto w-full overflow-auto rounded-xl border-2 border-blue-store bg-white p-2 shadow-lg"
+                                id="list-distritos">
+                                <li class="itemOptionDistrito cursor-default truncate rounded-xl px-4 py-2.5 font-pluto-r text-sm text-zinc-700 hover:bg-zinc-100 md:text-base"
+                                    data-input="#municipio">
+                                    Selecciona un municipio
+                                </li>
+                            </ul>
+                        </div>
+                        @error('distrito')
+                            <span class="text-sm text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
                     <div class="flex w-full">
                         <div class="flex flex-1 flex-col gap-2">
                             <x-input-store type="text" name="address_line_1" label="Dirección (línea 1)"
