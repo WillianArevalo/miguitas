39b527dd4779168775ab196d8a3aca8cb3e91900
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-14 21:22:57 -0600

    feat: Add 'rol' prop to store selection component for dynamic input targeting

--- resources/views/layouts/__partials/ajax/store/select.blade.php
+++ resources/views/layouts/__partials/ajax/store/select.blade.php
@@ -1,7 +1,7 @@
-    @props(['options' => [], 'type' => ''])
+    @props(['options' => [], 'type' => '', 'rol' => 'user'])
     @foreach ($options as $value => $label)
-        <li class="itemOption{{ $type }} cursor-default truncate rounded-lg px-4 py-3 text-sm text-zinc-900 hover:bg-zinc-100"
-            title="{{ $label }}" data-value="{{ $value }}" data-input="#municipio">
+        <li class="itemOption{{ $type }}{{--  @if ($rol == 'admin') dark:text-white dark:hover:bg-zinc-900 @endif --}} cursor-default truncate rounded-lg px-4 py-3 text-sm text-zinc-900 hover:bg-zinc-100"
+            title="{{ $label }}" data-value="{{ $value }}" data-input="#{{ strtolower($type) }}">
             {{ $label }}
         </li>
     @endforeach
