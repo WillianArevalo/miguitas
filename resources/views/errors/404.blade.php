b070b4c90ca447f3757cb5e1b5264e7c4926b7c2
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-17 17:25:04 -0600

    feat: Update 404 error page button to use 'a' type for improved navigation

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
     <div class="relative h-screen w-full">
         <img src="{{ asset('img/logo.png') }}" alt="Logo" class="size-28 absolute left-0 top-0 m-10">
         <img src="{{ asset('img/404.png') }}" alt="Image page not found" class="h-full w-full object-cover">
-        <x-button-store href="{{ Route('home') }}" typeButton="primary" class="absolute bottom-0 right-0 m-10"
+        <x-button-store type="a" href="{{ Route('home') }}" typeButton="primary" class="absolute bottom-0 right-0 m-10"
             text="Volver al inicio" size="large" />
     </div>
 @endsection
