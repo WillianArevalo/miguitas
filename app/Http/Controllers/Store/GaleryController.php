98e94464d41df0e049796899f5464caab786b34b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-17 17:23:42 -0600

    feat: Add WhatsApp setting to GaleryController for improved contact options

--- app/Http/Controllers/Store/GaleryController.php
+++ app/Http/Controllers/Store/GaleryController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Store;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Settings;
 
 class GaleryController extends Controller
 {
     public function index()
     {
-        return view("store.galery.index");
+        $whatsApp = Settings::where("key", "store_whatsapp")->first();
+        return view("store.galery.index", compact("whatsApp"));
     }
 }
