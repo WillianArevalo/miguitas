8a0ec1b8d7f0dcc578be0ec4d2aee862adad7837
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-17 17:26:11 -0600

    feat: Improve your gallery design with new styles and content

--- resources/views/store/galery/index.blade.php
+++ resources/views/store/galery/index.blade.php
@@ -5,44 +5,16 @@
 @endpush
 @section('content')
     <div class="main-container">
-        <div class="header-container" style="background-image: url({{ asset('img/bg-image.png') }});">
-            <div class="icon">
-                <a href="index.html">
-                    <svg width="45" height="45" viewBox="0 0 226 380" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M46 46L172.322 172.322C182.085 182.085 182.085 197.915 172.322 207.678L46 334"
-                            stroke="#8fadff" stroke-width="91" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </a>
-            </div>
-            <h1>GALERÍA</h1>
-            <div class="sp"></div>
+        <div class="py-20 text-center" style="background-image: url({{ asset('img/bg-image.png') }});">
+            <h1 class="text-3xl font-bold text-white sm:text-4xl md:text-5xl">Galería</h1>
         </div>
-        <div class="content">
-            <h1 class="light-blue">Cada día recibimos fotos de clientes felices y satisfechos. </h1>
-            <h1 class="light-blue">Envíanos la tuya al WHATSAPP 7910-1241.</h1>
-            <div class="image-grid">
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
-                <div class="image-item"></div>
+        <div>
+            <div class="py-8 text-center">
+                <h1 class="text-xl text-blue-store">Cada día recibimos fotos de clientes felices y satisfechos. </h1>
+                <h1 class="mt-2 font-dine-r text-blue-store">Envíanos la tuya al WHATSAPP {{ $whatsApp->value }}</h1>
+            </div>
+            <div class="mx-auto w-3/4">
+
             </div>
         </div>
     </div>
