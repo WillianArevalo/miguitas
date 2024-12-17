71cc3cb8cf192fcf17b370ab2025669cae0933f8
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-17 17:25:30 -0600

    feat: Improved styling of option elements in store selection

--- resources/views/layouts/__partials/ajax/store/select.blade.php
+++ resources/views/layouts/__partials/ajax/store/select.blade.php
@@ -1,6 +1,6 @@
     @props(['options' => [], 'type' => '', 'rol' => 'user'])
     @foreach ($options as $value => $label)
-        <li class="itemOption{{ $type }}{{--  @if ($rol == 'admin') dark:text-white dark:hover:bg-zinc-900 @endif --}} cursor-default truncate rounded-lg px-4 py-3 text-sm text-zinc-900 hover:bg-zinc-100"
+        <li class="itemOption{{ $type }}{{--  @if ($rol == 'admin') dark:text-white dark:hover:bg-zinc-900 @endif --}} cursor-default truncate rounded-xl px-4 py-2.5 font-din-r text-sm text-zinc-700 hover:bg-zinc-100 md:text-base"
             title="{{ $label }}" data-value="{{ $value }}" data-input="#{{ strtolower($type) }}">
             {{ $label }}
         </li>
