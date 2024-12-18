d3f9e766751438a8345d1008a9990fae60dca382
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-18 16:46:40 -0600

    feat: Update about page styling with font adjustments and color changes

--- resources/views/store/about/index.blade.php
+++ resources/views/store/about/index.blade.php
@@ -14,8 +14,8 @@
                 <div class="flex flex-1 items-center justify-center">
                     <img src="{{ asset('img/conocenos-dog.png') }}" alt="Conocenos" class="h-80 w-80 object-cover">
                 </div>
-                <div class="flex flex-1 flex-col gap-4 px-4 text-sm text-zinc-700 sm:pe-20 md:text-base">
-                    <h1 class="text-4xl font-bold text-light-blue">MIGUITAS</h1>
+                <div class="flex flex-1 flex-col gap-4 px-4 font-dine-r text-sm text-zinc-700 sm:pe-20 md:text-base">
+                    <h1 class="font-pluto-r text-4xl font-bold text-blue-store">MIGUITAS</h1>
                     <p>MIGUITAS PET TREATS nace en 2015 como
                         una iniciativa para que toda la familia
                         comparta los mejores sabores y la mejor
@@ -33,7 +33,7 @@
                 </div>
             </div>
             <div class="mt-20 text-zinc-700">
-                <div class="mx-auto flex w-full flex-col gap-4 px-4 text-sm sm:w-1/2 sm:px-0 md:text-base">
+                <div class="mx-auto flex w-full flex-col gap-4 px-4 font-dine-r text-sm sm:w-1/2 sm:px-0 md:text-base">
                     <p>En Miguitas, horneamos todos los días para los consentidos de cuatro patitas. ¡Elaboramos pasteles,
                         pupcakes y galletas de diferentes sabores, tamaño y forma para que cada día puedas consentir a tu
                         perrito o gatito de la mejor manera: natural! Tenemos también otros snacks como jerky, meatballs y
@@ -51,7 +51,7 @@
                     </p>
                 </div>
                 <div class="mx-auto mt-10 w-full px-4 sm:w-4/5 md:w-3/5">
-                    <h2 class="text-lg font-bold text-light-blue sm:text-xl md:text-2xl">
+                    <h2 class="text-lg font-bold text-blue-store sm:text-xl md:text-2xl">
                         Cada producto es elaborado con pasión, cuidando el detalle de la frescura y calidad de cada uno de
                         los ingredientes.
                     </h2>
@@ -60,14 +60,14 @@
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">PAWTY PACKS</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     La mejor manera de celebrar a tu peludo en su cumpleaños, su adopción, su logro, etc
                                 </p>
                             </div>
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">JERKY</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     Res o pollo deshidratado.100% proteína. Sin nada artificial, sólo ingredientes frescos
                                     sin aditivos, pre-mezclas o fillers. ¡El treat perfecto para los carnívoros!
                                 </p>
@@ -75,7 +75,7 @@
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">PUPCAKES</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     La versión perruna individual de un pastel de celebración. Ingredientes frescos y máxima
                                     calidad. ¡Más prácticos, imposible!
                                 </p>
@@ -83,7 +83,7 @@
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">PUPSICLES</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     Paletas de distintos sabores para peludos, cero ingredientes alérgenos o esencias
                                     artificiales, perfectas para los que mueren de calor y les gusta lo helado
                                 </p>
@@ -94,7 +94,7 @@
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">COOKIES</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     Premia y varía la dieta de tu mascota con cualquiera de nuestros 9 sabores de galletas
                                     naturales
                                 </p>
@@ -102,7 +102,7 @@
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">ACCESORIOS</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     Gorros, pañoletas, velas, números de galleta y globos huellitas. ¡Tenemos el accesorio
                                     perfecto para su celebración!
                                 </p>
@@ -111,16 +111,15 @@
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">CAKES</h3>
-                                <p>¿Tu peludo está por cumplir años? Nosotros te hacemos el pastel para que celebres con él,
-                                    ¡naturalmente!
-
+                                <p class="font-dine-r">¿Tu peludo está por cumplir años? Nosotros te hacemos el pastel para
+                                    que celebres con él, ¡naturalmente!
                                 </p>
                             </div>
 
                             <div
                                 class="flex flex-col gap-2 rounded-2xl border border-zinc-200 p-4 text-sm shadow-md md:text-base">
                                 <h3 class="text-lg font-bold text-dark-pink sm:text-xl">KITTYCAKES</h3>
-                                <p>
+                                <p class="font-dine-r">
                                     Los gatos también son consentidos en Miguitas, nuestra receta de Kittycakes sorprenderá
                                     a este rey de la casa tan especial.
                                 </p>
@@ -129,7 +128,7 @@
                     </div>
                 </div>
                 <div class="mx-auto my-20 w-full px-4 md:w-1/2">
-                    <h2 class="text-lg font-bold text-light-blue sm:text-xl md:text-2xl">
+                    <h2 class="text-lg font-bold text-blue-store sm:text-xl md:text-2xl">
                         Creemos en la alimentación natural para el bienestar integral, y mayor longevidad de tu mascota.
                     </h2>
                     <div class="mt-4 flex items-center justify-center gap-4">
