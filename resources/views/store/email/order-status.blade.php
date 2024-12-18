5bdcc15405e1e08c0a1e33e63bc094d851a2489e
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-18 16:47:39 -0600

    feat: Redesigned the order status email template with improvements to style and HTML structure

--- resources/views/store/email/order-status.blade.php
+++ resources/views/store/email/order-status.blade.php
@@ -1,21 +1,34 @@
-@extends('layouts.login-template')
-@section('title', 'Miguitas | Verifica tu correo electrónico')
-@section('content')
-    <div class="flex h-screen flex-col items-center justify-center gap-y-4 bg-white">
-        <h1 class="text-center text-2xl font-bold text-blue-store">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Miguitas - Costo de envío de tu pedido</title>
+</head>
+
+<body style="font-family: 'Poppins', sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; text-align: center;">
+    <div style="background-color: #ffffff; text-align: center; padding: 1rem; border-radius: 0.5rem;">
+        <h1 style="text-align: center; font-size: 1.5rem; font-weight: bold; color: #6e83be;">
             Miguitas - Estado {{ $status }} de tu pedido
         </h1>
-        <img src="{{ asset('img/logo.png') }}" alt="logo" class="size-20">
+        <img src="{{ asset('img/logo.png') }}" alt="logo" style="width: 5rem; height: 5rem;">
         @if ($number_order)
-            <h2 class="w-[500px] text-center text-2xl font-bold text-blue-store">
+            <h2 style="width: 500px; text-align: center; font-size: 1.5rem; font-weight: bold; color: #6e83be;">
                 Hola Willian, el estado de tu pedido #{{ $number_order }} ha sido actualizado.
             </h2>
-            <x-button-store type="a" target="_blank" typeButton="primary" href="{{ route('orders.show', $number_order) }}"
-                text="Ver pedido" />
-            <p class="text-center font-dine-r text-zinc-500">
+            <a target="_blank" href="{{ route('orders.show', $number_order) }}"
+                style="display: inline-block; padding: 0.5rem 1rem; background-color: #6e83be; color: #ffffff; text-decoration: none; border-radius: 0.25rem; font-weight: bold;">
+                Ver pedido
+            </a>
+            <p style="text-align: center; font-family: 'Dine-R', sans-serif; color: #71717a;">
                 Si tienes alguna pregunta, no dudes en contactarnos. <br>
-                <a href="mailto:contacto@miguitas.com" class="text-blue-store">contacto@miguitas.com</a>
+                <a href="mailto:contacto@miguitas.com" style="color: #6e83be;">contacto@miguitas.com</a>
             </p>
         @endif
     </div>
-@endsection
+
+</body>
+
+</html>
