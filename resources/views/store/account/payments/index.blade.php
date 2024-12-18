37fc501560db330fc7ca5f24f48b2d7f3ad43b17
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-18 16:47:13 -0600

    feat: Improve the design of the payments section with style and text adjustments

--- resources/views/store/account/payments/index.blade.php
+++ resources/views/store/account/payments/index.blade.php
@@ -23,12 +23,11 @@
              </div>
          </div>
          @if ($payments->count() === 0)
-             <div
-                 class="mt-4 flex items-center justify-center gap-4 rounded-2xl border-2 border-dashed border-zinc-200 p-10">
-                 <x-icon-store icon="alert" class="h-8 w-8 text-blue-store" />
+             <div class="my-4 flex h-full items-center justify-center gap-4 rounded-2xl p-20">
+                 <x-icon-store icon="alert" class="size-5 text-blue-store" />
                  <div class="flex flex-col items-center gap-1">
-                     <p class="font-pluto-r text-sm text-zinc-500">
-                         No tienes ningún pago registrado
+                     <p class="font-dine-r text-sm text-zinc-500">
+                         No tienes ningún pago realizado
                      </p>
                  </div>
              </div>
@@ -68,7 +67,6 @@
                          <tbody class="divide-y divide-zinc-200 bg-white">
                              @foreach ($payments as $payment)
                                  <tr class="hover:bg-zinc-50">
-
                                      <td class="whitespace-nowrap px-4 py-4">
                                          <span class="font-secondary font-pluto-r text-sm text-zinc-500">
                                              {{ $payment->order->number_order }}
@@ -87,35 +85,31 @@
                                          @switch($payment->status)
                                              @case('pending')
                                                  <span
-                                                     class="rounded-full bg-yellow-100 px-4 py-1 font-dine-b text-xs font-medium text-yellow-700">
-                                                     Pediente
-                                                 </span>
-                                             @break
-
-                                             @case('sent')
-                                                 <span
-                                                     class="rounded-full bg-blue-100 px-4 py-1 font-dine-b text-xs font-medium text-blue-700">
-                                                     Enviado
+                                                     class="flex w-max items-center justify-center gap-1 rounded-full bg-yellow-100 px-2 py-1 font-dine-b text-xs font-medium text-yellow-700">
+                                                     <x-icon-store icon="clock" class="h-4 w-4 text-yellow-700" />
+                                                     Pendiente
                                                  </span>
                                              @break
 
                                              @case('approved')
                                                  <span
-                                                     class="rounded-full bg-green-100 px-4 py-1 font-dine-b text-xs font-medium text-green-700">
-                                                     Completado
+                                                     class="flex w-max items-center justify-center gap-1 rounded-full bg-green-100 px-2 py-1 font-dine-b text-xs font-medium text-green-700">
+                                                     <x-icon icon="check-circle" class="h-4 w-4 text-green-700" />
+                                                     Aprobado
                                                  </span>
                                              @break
 
-                                             @default
+                                             @case('rejected')
                                                  <span
-                                                     class="rounded-full bg-yellow-100 px-4 py-1 font-dine-b text-xs font-medium text-yellow-700">
-                                                     Pediente
+                                                     class="flex w-max items-center justify-center gap-1 rounded-full bg-red-100 px-2 py-1 font-dine-b text-xs font-medium text-red-700">
+                                                     <x-icon icon="check-circle" class="h-4 w-4 text-red-700" />
+                                                     Rechazado
                                                  </span>
                                              @break
                                          @endswitch
                                      </td>
                                      <td class="whitespace-nowrap px-4 py-4 text-sm">
-                                         Wompi
+                                         {{ $payment->paymentMethod->name }}
                                      </td>
                                      <td class="whitespace-nowrap px-4 py-4 text-sm">
                                          <div class="flex items-center gap-2">
