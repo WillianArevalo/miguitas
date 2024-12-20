97f4baff128aa70c1cbc7064dd11b3e2e0e075aa
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-19 21:21:56 -0600

    feat: Add conditional logic to display payment forms based on the selected method

--- resources/views/store/account/payments/create.blade.php
+++ resources/views/store/account/payments/create.blade.php
@@ -3,42 +3,107 @@
 @section('content')
     <div class="mx-auto flex w-full flex-col-reverse items-start gap-4 px-4 pb-6 md:flex-row lg:w-3/4">
         <div class="flex-[1.5]">
-            <h3 class="text-lg font-bold uppercase text-blue-store sm:text-xl md:text-2xl">
-                Información de pago
-            </h3>
-            <form id="payment-form" method="POST" action="{{ Route('payment.charge') }}" class="mt-4 flex flex-col gap-2">
-                @csrf <!-- Aquí se agrega el token CSRF -->
-                <div class="flex flex-col gap-2">
-                    <x-input-store type="text" label="Nombre del titular de la tarjeta" name="name"
-                        placeholder="Ingresa el nombre del titular de la tarjeta" id="name" />
-                    <div id="error-message-name" class="font-dine-r text-red-400"></div>
-                </div>
+            @if ($paymentMethod->name === 'Tarjeta de crédito')
+                <h3 class="text-lg font-bold uppercase text-blue-store sm:text-xl md:text-2xl">
+                    Información de pago
+                </h3>
+                <form id="payment-form" method="POST" action="{{ Route('payment.charge') }}" class="mt-4 flex flex-col gap-2">
+                    @csrf <!-- Aquí se agrega el token CSRF -->
+                    <div class="flex flex-col gap-2">
+                        <x-input-store type="text" label="Nombre del titular de la tarjeta" name="name"
+                            placeholder="Ingresa el nombre del titular de la tarjeta" id="name" />
+                        <div id="error-message-name" class="font-dine-r text-red-400"></div>
+                    </div>
 
-                <div id="card-element" class="rounded-xl border border-zinc-300 px-4 py-6 font-dinc-r">
-                    <!-- Elemento donde se inserta el campo de la tarjeta -->
-                </div>
-                <div id="error-message" class="font-dine-r text-red-400"></div>
-                <div class="flex items-center gap-2">
-                    <input type="checkbox" name="pending_payment" id="pending_payment" value="1"
-                        class="rounded-md border-2 border-zinc-300 p-2 text-blue-store focus:ring-blue-store">
-                    <label class="block font-dine-r text-sm font-medium text-zinc-600">
-                        La transacción es realizada por el titular de la tarjeta de crédito/débito
-                    </label>
-                </div>
-                <div>
-                    <p class="font-dine-r text-sm font-normal text-zinc-600">
-                        Sus datos personales se utilizarán para procesar su pedido, respaldar su experiencia en este sitio
-                        web y para los fines descritos en nuestra <a
-                            href="{{ Route('terms-and-conditions', 'politicas-de-privacidad') }}"
-                            class="text-dark-pink underline">política de privacidad</a>.Conoce
-                        nuestra política de <a href="{{ Route('terms-and-conditions', 'garantias-y-cambios') }}"
-                            class="text-dark-pink underline">garantía y cambios</a>.
-                    </p>
-                </div>
-                <div class="mt-4 flex items-center justify-center">
-                    <x-button-store type="submit" text="Pagar" typeButton="primary" />
-                </div>
-            </form>
+                    <div id="card-element" class="rounded-xl border border-zinc-300 px-4 py-6 font-dinc-r">
+                        <!-- Elemento donde se inserta el campo de la tarjeta -->
+                    </div>
+                    <div id="error-message" class="font-dine-r text-red-400"></div>
+                    <div class="flex items-center gap-2">
+                        <input type="checkbox" name="pending_payment" id="pending_payment" value="1"
+                            class="rounded-md border-2 border-zinc-300 p-2 text-blue-store focus:ring-blue-store">
+                        <label class="block font-dine-r text-sm font-medium text-zinc-600">
+                            La transacción es realizada por el titular de la tarjeta de crédito/débito
+                        </label>
+                    </div>
+                    <div>
+                        <p class="font-dine-r text-sm font-normal text-zinc-600">
+                            Sus datos personales se utilizarán para procesar su pedido, respaldar su experiencia en este
+                            sitio
+                            web y para los fines descritos en nuestra <a
+                                href="{{ Route('terms-and-conditions', 'politicas-de-privacidad') }}"
+                                class="text-dark-pink underline">política de privacidad</a>.Conoce
+                            nuestra política de <a href="{{ Route('terms-and-conditions', 'garantias-y-cambios') }}"
+                                class="text-dark-pink underline">garantía y cambios</a>.
+                        </p>
+                    </div>
+                    <div class="mt-4 flex items-center justify-center">
+                        <x-button-store type="submit" text="Pagar" typeButton="primary" />
+                    </div>
+                </form>
+            @endif
+
+            @if ($paymentMethod->name === 'Transferencia bancaria')
+                <h3 class="text-lg font-bold uppercase text-blue-store sm:text-xl md:text-2xl">
+                    Datos del banco
+                </h3>
+                <form action="{{ Route('bank-transfers.store') }}" method="POST" enctype="multipart/form-data"
+                    id="form-file-payment">
+                    @csrf
+                    <input type="hidden" name="bank_detail_id" value="{{ $bank->id }}">
+                    <div class="mt-4 flex flex-col gap-2">
+                        <p class="font-dine-r text-sm text-zinc-600">
+                            Por favor realiza una transferencia a la siguiente cuenta bancaria:
+                        </p>
+                        <div class="flex flex-col gap-2 rounded-xl border-2 border-dashed border-zinc-300 p-4">
+                            <div class="flex gap-2">
+                                <h4 class="font-dine-r font-semibold text-zinc-800">Banco:</h4>
+                                <p class="font-dine-r text-zinc-600">{{ $bank->bank_name }}</p>
+                            </div>
+                            <div class="flex gap-2">
+                                <h4 class="font-dine-r font-semibold text-zinc-800">Número de cuenta:</h4>
+                                <p class="font-dine-r text-zinc-600">{{ $bank->account_number }}</p>
+                            </div>
+                            <div class="flex gap-2">
+                                <h4 class="font-dine-r font-semibold text-zinc-800">Nombre del titular de la cuenta:</h4>
+                                <p class="font-dine-r text-zinc-600">{{ $bank->account_holder }}</p>
+                            </div>
+                            <div class="flex gap-2">
+                                <h4 class="font-dine-r font-semibold text-zinc-800">Tipo de cuenta:</h4>
+                                <p class="font-dine-r text-zinc-600">
+                                    {{ $bank->account_type === 'savings' ? 'Cuenta de ahorro' : 'Cuenta corriente' }}</p>
+                            </div>
+                            <p class="font-dine-r text-sm text-zinc-600">
+                                Realizada la transferencia, por favor adjunta el comprobante de pago:
+                            </p>
+                            <div class="flex items-center justify-between gap-2 rounded-full border border-zinc-300 p-1"
+                                id="container-file">
+                                <div class="flex items-center gap-2">
+                                    <label
+                                        class="disabled:text-zincaret-red-400 flex w-max cursor-pointer items-center justify-center gap-2 rounded-full border border-zinc-300 bg-white px-4 py-2 font-din-r text-sm text-zinc-600 transition-colors duration-300 hover:bg-zinc-200/50 disabled:cursor-not-allowed disabled:bg-zinc-100/50">
+                                        <input type="file" name="file" id="proof-payment" class="hidden"
+                                            accept=".jpg, .jpeg, .png, .pdf">
+                                        Adjuntar archivo
+                                    </label>
+                                    <p id="file-name" class="font-dine-r text-xs text-zinc-600">
+                                        Formatos permitidos: .jpg, .jpeg, .png,.pdf
+                                    </p>
+                                </div>
+                                <button id="remove-file" class="me-2 hidden">
+                                    <x-icon icon="x" class="h-4 w-4 text-red-500" />
+                                </button>
+                            </div>
+                            @error('file')
+                                <p class="font-dine-r text-sm text-red-400">{{ $message }}</p>
+                            @enderror
+                            <div class="mt-2 flex items-center justify-center">
+                                <x-button-store id="addBankTransfer" text="Enviar comprobante" typeButton="primary"
+                                    type="button" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            @endif
         </div>
         <div class="w-full flex-1">
             <div class="rounded-2xl border border-zinc-300 p-4">
